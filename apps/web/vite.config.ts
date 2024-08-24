318e01b04115997b6a81cece143b700cc5288b97
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 16:24:59 -0700

    new react base

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import runtimeEnv from "vite-plugin-runtime-env";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [solid(), runtimeEnv()],
+  plugins: [react()],
 });
