45be58b6d5eaa5e6b56f1bc265fbd5ffd39f08f1
Turbobot <turbobot@vercel.com>
2024-08-24 12:57:57 -0700

    skeleton: add turbo monorepo structure

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["@chunkmydocs/eslint-config/index.js"],
+};
---
+++ turbo-ex/apps/web/src/main.ts
@@ -0,0 +1,22 @@
+import { Header } from "@repo/ui/header";
+import "./style.css";
+import typescriptLogo from "/typescript.svg";
+import { Counter } from "@repo/ui/counter";
+import { setupCounter } from "@repo/ui/setup-counter";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    ${Header({ title: "Web" })}
+    <div class="card">
+      ${Counter()}
+    </div>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
