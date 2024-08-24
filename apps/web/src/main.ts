4e77ae909a69883d86b4635d8f468458659d2960
Turbobot <turbobot@vercel.com>
2024-08-24 12:57:57 -0700

    skeleton: add turbo monorepo structure

---
+++ apps/web/src/main.ts
@@ -0,0 +1,22 @@
+import { Header } from "@chunkmydocs/ui/header";
+import "./style.css";
+import typescriptLogo from "/typescript.svg";
+import { Counter } from "@chunkmydocs/ui/counter";
+import { setupCounter } from "@chunkmydocs/ui/setup-counter";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    ${Header({ title: "Web" })}
+    <div class="card">
+      ${Counter()}
+    </div>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
