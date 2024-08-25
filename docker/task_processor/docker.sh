3ff17eb17af02e0dc596befab430d0afcabda549
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-08-25 16:27:48 -0700

    updated rust version

--- docker/task_processor/docker.sh
+++ docker/task_processor/docker.sh
@@ -6,7 +6,7 @@ CURRENT_DIR=$(pwd)
 cd ../../
 
 # Define the Docker image name as a variable
-DOCKER_IMAGE_NAME="luminainc/chunkmydocs"
+DOCKER_IMAGE_NAME="luminainc/task-processor"
 
 # Get the current commit SHA
 SHA=$(git rev-parse --short HEAD)
