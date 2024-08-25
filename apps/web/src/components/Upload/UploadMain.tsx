8cc92be4b5f187b64c5cdf5168c992e967d182ac
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 21:32:14 -0700

    Completed homepage static setup and phone sizing

--- apps/web/src/components/Upload/UploadMain.tsx
+++ apps/web/src/components/Upload/UploadMain.tsx
@@ -1,6 +1,7 @@
 import { Flex, Text } from "@radix-ui/themes";
 import Upload from "./Upload";
 import "./UploadMain.css";
+import BetterButton from "../BetterButton/BetterButton";
 
 export default function UploadMain() {
   return (
@@ -13,16 +14,33 @@ export default function UploadMain() {
         mt="40px"
         className="toggle-container"
       >
-        <Flex direction="column" height="100%" className="toggle-active">
-          <Text size="4" weight="bold">
+        <Flex
+          direction="column"
+          height="100%"
+          justify="center"
+          className="toggle-active"
+        >
+          <Text size="4" weight="medium">
             Fast
           </Text>
         </Flex>
-        <Flex direction="column" height="100%" className="toggle">
-          <Text size="4" weight="bold">
+        <Flex
+          direction="column"
+          height="100%"
+          justify="center"
+          className="toggle"
+        >
+          <Text size="4" weight="medium">
             High Quality
           </Text>
         </Flex>
+      </Flex>
+      <Flex direction="row" width="100%" mt="32px">
+        <BetterButton padding="16px 64px">
+          <Text size="4" weight="medium">
+            Run
+          </Text>
+        </BetterButton>
       </Flex>
     </Flex>
   );
