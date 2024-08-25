0e08ddd0cd602e24fc7293b48df154c65e896da8
cdxker <fdenzell@gmail.com>
2024-08-24 18:45:57 -0700

    feat: added in baseline viewer panels

--- apps/web/src/pages/Viewer.tsx
+++ apps/web/src/pages/Viewer.tsx
@@ -1,12 +1,29 @@
-import BetterButton from "../components/BetterButton";
 import { Text } from "@radix-ui/themes";
 
-export const Viewer = () => {
-  return (
-    <BetterButton>
-      <Text size="9" weight="medium">
-        Hello Viewer
-      </Text>
-    </BetterButton>
-  );
-};
+export const Viewer = () => <div className="flex border-t border-cyan-6 w-full justify-between">
+  <div className="border-l w-full h-full">
+    <Text>I'm pdf bro</Text>
+  </div>
+  <div className="border-l w-full p-4 space-y-4">
+    {[0, 1, 2, 3, 4].map(() => {
+
+      return (
+        <div className="border-2">
+
+          <div className="flex border-b-2 justify-between">
+            <Text> Header 2x </Text>
+            <div className="flex">
+              <Text> Markdown </Text>
+              <Text> Json</Text>
+            </div>
+          </div>
+
+          <div className="p-40">
+            <Text> What is up bro, I'ma chunk </Text>
+          </div>
+        </div>
+      );
+    })}
+  </div>
+
+</div>;
