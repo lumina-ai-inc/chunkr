fff39a8c1b06e56027f12d62f9c478e0febf7f76
cdxker <fdenzell@gmail.com>
2024-08-25 13:57:49 -0700

    feat: using react-pdf

--- apps/web/src/components/PDF/PDF.tsx
+++ apps/web/src/components/PDF/PDF.tsx
@@ -1,83 +1,89 @@
-import { useState, useEffect, useId } from "react";
-import { Document, Page } from 'react-pdf';
-import * as pdfjs from 'pdfjs-dist';
+import { useCallback, useEffect, useState } from 'react';
+import { useResizeObserver } from '@wojtekmaj/react-hooks';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-function REACTPDF() {
-    const [numPages, setNumPages] = useState<number>();
-    const [pageNumber, setPageNumber] = useState<number>(1);
+import type { PDFDocumentProxy } from 'pdfjs-dist';
+import { ScrollArea } from '@radix-ui/themes';
 
-    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-        setNumPages(numPages);
-    }
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
-    return (
-        <div>
-            <Document file="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-                <Page pageNumber={pageNumber} />
-            </Document>
-            <p>
-                Page {pageNumber} of {numPages}
-            </p>
-        </div>
-    );
-}
-
-export const PDF = () => {
-    // const url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf'
-    const id = useId();
-    //
-    // useEffect(() => {
-    //
-    //     initDocument()
-    //         .catch(console.error)
-    // }, []);
-    //
-    // const initDocument = async () => {
-    //     console.log("prepdf");
-    //
-    //     pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
-    //
-    //     const pdf = await pdfjs.getDocument(url).promise;
-    //
-    //     const pageNumber = 1;
-    //     const page = await pdf.getPage(pageNumber);
-    //
-    //     const scale = 1.5;
-    //     const viewport = page.getViewport({ scale: scale });
-    //
-    //     // Prepare canvas using PDF page dimensions
-    //     const canvas: HTMLCanvasElement = document.getElementById(id) as HTMLCanvasElement;
-    //     const context = canvas.getContext('2d')!;
-    //     canvas.height = viewport.height;
-    //     canvas.width = viewport.width;
-    //
-    //     // Render PDF page into canvas context
-    //     const renderContext = {
-    //         canvasContext: context,
-    //         viewport: viewport
-    //     };
-    //     const renderTask = page.render(renderContext);
-    //     await renderTask.promise;
-    //     //
-    //     // canvas.width = Math.floor(viewport.width);
-    //     // canvas.height = Math.floor(viewport.height);
-    //     // canvas.style.width = Math.floor(viewport.width) + "px";
-    //     // canvas.style.height = Math.floor(viewport.height) + "px";
-    //     // console.log("pdf");
-    // };
-
-    const showA = false;
-
-    return (
-        <div>
-            {showA &&
-                <canvas id={id}>
-                </canvas>
-            }
-            {!showA &&
-                <REACTPDF />
-            }
-        </div>
-    );
+const options = {
+  cMapUrl: '/cmaps/',
+  standardFontDataUrl: '/standard_fonts/',
 };
 
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+type PDFFile = string | File | null;
+
+export function PDF() {
+  const [file, setFile] = useState<PDFFile>('./sample.pdf');
+  const [numPages, setNumPages] = useState<number>();
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+
+  const url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  useEffect(() => {
+    setFile(url);
+  });
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div>
+      <div>
+        <div ref={setContainerRef}>
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            <ScrollArea
+              scrollbars='both'
+              type='always'
+              style={{
+                height: "calc(100vh - 90px)",
+                padding: "24px",
+                maxWidth: "calc(100% - 2em)"
+              }}
+            >
+              <div className='flex flex-col'
+                style={{
+                  height: "calc(100vh - 90px)",
+                  padding: "24px",
+                  maxWidth: "calc(100% - 2em)"
+                }}>
+                {Array.from(new Array(numPages), (_el, index) => (
+                  <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                    width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+                    onClick={(event) => {
+                      console.log("hi", event)
+                    }}
+                  />
+                ))}
+              </div>
+            </ScrollArea>
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+}
