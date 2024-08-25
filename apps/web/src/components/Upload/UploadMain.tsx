9e3bac0920419216f244c8398254fad47645f57d
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 20:45:07 -0700

    Progress on homepage

--- apps/web/src/components/Upload/UploadMain.tsx
+++ apps/web/src/components/Upload/UploadMain.tsx
@@ -1,10 +1,29 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Text } from "@radix-ui/themes";
 import Upload from "./Upload";
+import "./UploadMain.css";
 
 export default function UploadMain() {
   return (
     <Flex direction="column" width="100%">
       <Upload />
+      <Flex
+        direction="row"
+        height="64px"
+        width="100%"
+        mt="40px"
+        className="toggle-container"
+      >
+        <Flex direction="column" height="100%" className="toggle-active">
+          <Text size="4" weight="bold">
+            Fast
+          </Text>
+        </Flex>
+        <Flex direction="column" height="100%" className="toggle">
+          <Text size="4" weight="bold">
+            High Quality
+          </Text>
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
