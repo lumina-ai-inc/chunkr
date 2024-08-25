6c07870ed9633cf6278a72eb937e9e945d1c311e
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 19:40:20 -0700

    Added header on homepage

---
+++ apps/web/src/components/Upload/UploadMain.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@radix-ui/themes";
+import Upload from "./Upload";
+
+export default function UploadMain() {
+  return (
+    <Flex direction="column" width="100%">
+      <Upload />
+    </Flex>
+  );
+}
