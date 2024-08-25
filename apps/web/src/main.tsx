57f97f9478960e5e5b29aeb6e9139633738eb1e1
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 17:15:46 -0700

    Added radix themes stuff and removed App.tsc App.css files

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Home } from "./pages/Home.tsx";
 import { Viewer } from "./pages/Viewer.tsx";
-import "./index.css";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@radix-ui/themes/styles.css";
+
+import { Theme } from "@radix-ui/themes";
 
 const router = createBrowserRouter([
   {
@@ -18,7 +21,13 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Theme
+      scaling="100%"
+      accentColor="sky"
+      panelBackground="solid"
+      style={{ height: "100%" }}
+    >
+      <RouterProvider router={router} />
+    </Theme>
   </React.StrictMode>
 );
-
--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,3 +1,16 @@
+import { Text, Button, Flex } from "@radix-ui/themes";
+
 export const Home = () => {
-  return <div>Hello Home</div>;
+  return (
+    <Flex
+      direction="column"
+      gap="2"
+      style={{ backgroundColor: "var(--sky-3)" }}
+    >
+      <Text size="9" weight="medium" style={{ color: "var(--sky-12)" }}>
+        Hello from Radix Themes :)
+      </Text>
+      <Button>Let's go</Button>
+    </Flex>
+  );
 };
