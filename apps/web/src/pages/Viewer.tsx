dfc44cc63d492a62cf3d4728c29f3029e16b1bbe
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 18:14:04 -0700

    Added cyan color scale classes

--- apps/web/src/pages/Viewer.tsx
+++ apps/web/src/pages/Viewer.tsx
@@ -1,5 +1,12 @@
-import StyledButton from "../components/StyledButton";
+import BetterButton from "../components/BetterButton";
+import { Text } from "@radix-ui/themes";
 
 export const Viewer = () => {
-  return <StyledButton>Hello Viewer</StyledButton>;
+  return (
+    <BetterButton>
+      <Text size="9" weight="medium" className="cyan-8">
+        Hello Viewer
+      </Text>
+    </BetterButton>
+  );
 };
