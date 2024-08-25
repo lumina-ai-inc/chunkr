a9948091e06367ba779ccd5782a4f194c0c4acde
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-08-25 13:55:19 -0700

    local pg

--- extraction/src/data/schema.rs
+++ extraction/src/data/schema.rs
@@ -47,9 +47,73 @@ diesel::table! {
     }
 }
 
+diesel::table! {
+    ingestion_files (file_id) {
+        id -> Nullable<Text>,
+        file_id -> Text,
+        task_id -> Nullable<Text>,
+        file_name -> Nullable<Text>,
+        file_size -> Nullable<Int8>,
+        page_count -> Nullable<Int4>,
+        created_at -> Nullable<Timestamptz>,
+        status -> Nullable<Text>,
+        input_location -> Nullable<Text>,
+        output_location -> Nullable<Text>,
+        expiration_time -> Nullable<Timestamptz>,
+        model -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    ingestion_tasks (task_id) {
+        task_id -> Text,
+        file_count -> Nullable<Int4>,
+        total_size -> Nullable<Int8>,
+        total_pages -> Nullable<Int4>,
+        created_at -> Nullable<Timestamptz>,
+        finished_at -> Nullable<Text>,
+        api_key -> Nullable<Text>,
+        status -> Nullable<Text>,
+        url -> Nullable<Text>,
+        model -> Nullable<Text>,
+        expiration_time -> Nullable<Timestamptz>,
+        message -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    ingestion_usage (task_id) {
+        task_id -> Text,
+        user_id -> Nullable<Text>,
+        api_key -> Nullable<Text>,
+        usage_type -> Nullable<Text>,
+        usage -> Nullable<Float8>,
+        usage_unit -> Nullable<Text>,
+        created_at -> Nullable<Timestamptz>,
+    }
+}
+
+diesel::table! {
+    usage_limit (id) {
+        id -> Int4,
+        user_id -> Nullable<Text>,
+        usage_type -> Nullable<Text>,
+        usage_limit -> Nullable<Float8>,
+        usage_unit -> Nullable<Text>,
+        created_at -> Nullable<Timestamptz>,
+    }
+}
+
+diesel::joinable!(ingestion_files -> ingestion_tasks (task_id));
+diesel::joinable!(ingestion_tasks -> api_keys (api_key));
+
 diesel::allow_tables_to_appear_in_same_query!(
     api_key_limit,
     api_key_usage,
     api_keys,
     api_users,
+    ingestion_files,
+    ingestion_tasks,
+    ingestion_usage,
+    usage_limit,
 );
