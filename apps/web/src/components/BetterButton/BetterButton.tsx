09d49efaa8af894e5677ce375bf6babb5723a3dd
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 19:48:52 -0700

    Added onClick prop to Betterbutton

--- apps/web/src/components/BetterButton/BetterButton.tsx
+++ apps/web/src/components/BetterButton/BetterButton.tsx
@@ -4,15 +4,18 @@ export default function BetterButton({
   children,
   padding = "4px 12px",
   active = false,
+  onClick,
 }: {
   children: React.ReactNode;
   padding?: string;
   active?: boolean;
+  onClick?: () => void;
 }) {
   return (
     <button
       className={`button-resting ${active ? "button-active" : ""}`}
       style={{ padding }}
+      onClick={onClick}
     >
       {children}
     </button>
