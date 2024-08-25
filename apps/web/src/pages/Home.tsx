874343b6e720504843d6d8320ce63258cd973dc5
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 18:07:42 -0700

    Updates

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import StyledButton from "../components/StyledButton";
 
 export const Home = () => {
   return (
-    <Text size="9" weight="medium">
+    <Text size="9" weight="medium" style={{ color: "var(--cyan-3)" }}>
       <StyledButton>Hello</StyledButton>
     </Text>
   );
