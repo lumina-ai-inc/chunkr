e73e0767159dd890262947f2d8ce72a08ba953d0
Mehul Chadda <mehulchadda@macbook-pro.mynetworksettings.com>
2024-08-24 19:40:20 -0700

    Added header on homepage

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Text } from "@radix-ui/themes";
 import "./Home.css";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
+import UploadMain from "../components/Upload/UploadMain";
 
 export const Home = () => {
   return (
@@ -14,6 +15,22 @@ export const Home = () => {
       className="pulsing-background"
     >
       <Header />
+      <Flex className="hero-container" direction="row">
+        <Flex className="text-container" direction="column" gap="4">
+          <Text
+            className="cyan-1"
+            size="9"
+            weight="bold"
+            trim="both"
+            style={{ maxWidth: "542px", fontSize: "72px", lineHeight: "96px" }}
+          >
+            Open Source Data Ingestion for LLMs & RAG
+          </Text>
+        </Flex>
+        <Flex className="module-container" direction="column" gap="4">
+          <UploadMain />
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
