de5e66d3c86487dd9da292bcb57b887ec808eb3a
Mehul Chadda <mehul@lumina.sh>
2024-08-27 19:39:15 -0700

    Updates

--- apps/web/src/pages/Home/Home.tsx
+++ apps/web/src/pages/Home/Home.tsx
@@ -24,7 +24,7 @@ export const Home = () => {
               trim="both"
               className="cyan-2 hero-title"
             >
-              Source Available Data Ingestion for LLMs & RAG
+              Source Available PDF Ingestion for LLMs & RAG
             </Text>
             <Text
               className="cyan-2"
@@ -39,74 +39,142 @@ export const Home = () => {
               done with API/self-hosting.
             </Text>
             <Flex
-              className="credit-container"
-              direction="column"
-              gap="16px"
-              mt="40px"
+              direction="row"
+              align="start"
+              className="credit-main-container"
             >
-              <Text size="6" weight="bold" className="cyan-2">
-                Powered by
-              </Text>
-              <Flex direction="row" gap="32px" align="center">
-                <a
-                  href="https://www.lumina.sh/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Flex
-                    gap="12px"
-                    align="center"
-                    justify="center"
-                    className="credit-button"
+              <Flex
+                className="credit-container"
+                direction="column"
+                gap="16px"
+                mt="40px"
+              >
+                <Text size="6" weight="bold" className="cyan-3">
+                  Powered by
+                </Text>
+                <Flex direction="row" gap="32px" align="center">
+                  <a
+                    href="https://www.lumina.sh/"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="32"
-                      height="32"
-                      viewBox="0 0 32 32"
-                      fill="none"
+                    <Flex
+                      gap="12px"
+                      align="center"
+                      justify="center"
+                      className="credit-button"
                     >
-                      <circle cx="16" cy="16" r="16" fill="#D1F0FA" />
-                      <path
-                        d="M9.57141 8.28572H22.4286M10.8571 8.28572V22C10.8571 22.4547 11.0377 22.8907 11.3592 23.2122C11.6807 23.5337 12.1168 23.7143 12.5714 23.7143H19.4286C19.8832 23.7143 20.3192 23.5337 20.6407 23.2122C20.9622 22.8907 21.1428 22.4547 21.1428 22V8.28572M10.8571 17.7143H21.1428"
-                        stroke="#60B3D7"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                    <Text size="7" weight="bold" className="cyan-1">
-                      Lumina
-                    </Text>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 32 32"
+                        fill="none"
+                      >
+                        <circle cx="16" cy="16" r="16" fill="#D1F0FA" />
+                        <path
+                          d="M9.57141 8.28572H22.4286M10.8571 8.28572V22C10.8571 22.4547 11.0377 22.8907 11.3592 23.2122C11.6807 23.5337 12.1168 23.7143 12.5714 23.7143H19.4286C19.8832 23.7143 20.3192 23.5337 20.6407 23.2122C20.9622 22.8907 21.1428 22.4547 21.1428 22V8.28572M10.8571 17.7143H21.1428"
+                          stroke="#60B3D7"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                      <Text size="7" weight="bold" className="cyan-1">
+                        Lumina
+                      </Text>
+                    </Flex>
+                  </a>
+                </Flex>
+              </Flex>
+              <Flex
+                className="credit-container"
+                direction="column"
+                gap="16px"
+                mt="40px"
+              >
+                <Text size="6" weight="bold" className="cyan-3">
+                  Core Contributors
+                </Text>
+                <Flex
+                  direction="row"
+                  gap="32px"
+                  align="center"
+                  style={{
+                    padding: "12px 18px",
+                    borderRadius: "8px",
+                    border: "1px solid var(--cyan-4)",
+                    backgroundColor: "#061d22",
+                    height: "62px",
+                  }}
+                >
+                  <Flex direction="row" gap="16px">
+                    <a
+                      href="https://github.com/skeptrunedev"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Flex height="32px" width="32px" className="credit-dp">
+                        <img
+                          src="src/assets/Nick.jpeg"
+                          alt="Nick from Trieve"
+                          style={{ borderRadius: "50%" }}
+                        />
+                      </Flex>
+                    </a>
+                    <a
+                      href="https://github.com/cdxker"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Flex height="32px" width="32px" className="credit-dp">
+                        <img
+                          src="src/assets/Denzell.png"
+                          alt="Denzell from Trieve"
+                          style={{ borderRadius: "50%" }}
+                        />
+                      </Flex>
+                    </a>
+                    <a
+                      href="https://github.com/akhileshsharma99"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Flex height="32px" width="32px" className="credit-dp">
+                        <img
+                          src="src/assets/Akhilesh.jpeg"
+                          alt="Akhilesh from Lumina"
+                          style={{ borderRadius: "50%" }}
+                        />
+                      </Flex>
+                    </a>
+                    <a
+                      href="https://github.com/m-chadda"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Flex height="32px" width="32px" className="credit-dp">
+                        <img
+                          src="src/assets/Mehul.jpeg"
+                          alt="Mehul from Lumina"
+                          style={{ borderRadius: "50%" }}
+                        />
+                      </Flex>
+                    </a>
+                    <a
+                      href="https://github.com/ishaan99k"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Flex height="32px" width="32px" className="credit-dp">
+                        <img
+                          src="src/assets/Ishaan.png"
+                          alt="Ishaan from Lumina"
+                          style={{ borderRadius: "50%" }}
+                        />
+                      </Flex>
+                    </a>
                   </Flex>
-                </a>
-                <Flex direction="row" gap="16px">
-                  <a
-                    href="https://github.com/skeptrunedev"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Flex height="32px" width="32px" className="credit-dp">
-                      <img
-                        src="src/assets/Nick.jpeg"
-                        alt="Nick from Trieve"
-                        style={{ borderRadius: "50%" }}
-                      />
-                    </Flex>
-                  </a>
-                  <a
-                    href="https://github.com/cdxker"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Flex height="32px" width="32px" className="credit-dp">
-                      <img
-                        src="src/assets/Denzell.png"
-                        alt="Denzell from Trieve"
-                        style={{ borderRadius: "50%" }}
-                      />
-                    </Flex>
-                  </a>
                 </Flex>
               </Flex>
             </Flex>
