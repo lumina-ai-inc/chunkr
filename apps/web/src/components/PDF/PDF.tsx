cb7334cea256426e117114b58d7374e1c723aa8f
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-08-28 19:10:31 -0700

    fixed pdf stuff

--- apps/web/src/components/PDF/PDF.tsx
+++ apps/web/src/components/PDF/PDF.tsx
@@ -2,15 +2,21 @@ import { useEffect, useRef, useState } from "react";
 import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
-
-import type { PDFDocumentProxy } from "pdfjs-dist";
 import { ScrollArea } from "@radix-ui/themes";
 import { Chunk } from "../../models/chunk.model";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`,
+//   import.meta.url
+// ).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   "pdfjs-dist/build/pdf.worker.min.mjs",
+//   import.meta.url
+// ).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136//pdf.min.mjs`; 
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@4.4.168/+esm`;
+
 
 const options = {
   cMapUrl: "/cmaps/",
@@ -31,21 +37,18 @@ type SegmentType =
   | "Footnote"
   | "Caption";
 
-export function PDF({ content, pdfFile }: { content: Chunk[]; pdfFile: File }) {
-  const file = pdfFile;
+export function PDF({ content, inputFileUrl }: { content: Chunk[]; inputFileUrl: string }) {
   const [numPages, setNumPages] = useState<number>();
 
   const segments = content;
 
-  function onDocumentLoadSuccess({
-    numPages: nextNumPages,
-  }: PDFDocumentProxy): void {
-    setNumPages(nextNumPages);
+  function onDocumentLoadSuccess(document: pdfjs.PDFDocumentProxy): void {
+    setNumPages(document.numPages);
   }
-
+  
   return (
     <Document
-      file={file}
+      file={inputFileUrl}
       onLoadSuccess={onDocumentLoadSuccess}
       options={options}
     >
@@ -58,7 +61,7 @@ export function PDF({ content, pdfFile }: { content: Chunk[]; pdfFile: File }) {
       >
         <div className="flex flex-col items-center space-y-2" style={{}}>
           {Array.from(new Array(numPages), (_el, index) => {
-            return <CurrentPage index={index} segments={segments} />;
+            return <CurrentPage key={index} index={index} segments={segments} />;
           })}
         </div>
       </ScrollArea>
@@ -77,20 +80,23 @@ function CurrentPage({ index, segments }: { index: number; segments: any }) {
     return inSegmentChunks;
   });
 
-  const pageRef = useRef(null);
+  const pageRef = useRef<HTMLDivElement>(null);
   const [containerWidth, setContainerWidth] = useState(0);
 
   useEffect(() => {
-    const pageDiv = pageRef.current.querySelector(".react-pdf__Page");
-    setContainerWidth(pageDiv.width);
-  });
-
+    if (!pageRef.current) return;
+    const pageDiv = pageRef.current.querySelector<HTMLDivElement>(".react-pdf__Page");
+    if (pageDiv) {
+      setContainerWidth(pageDiv.offsetWidth);
+    }
+  }, [pageRef]);
   return (
     <div ref={pageRef} className="flex relative items-center">
       <Page
         key={`page_${index + 1}`}
         pageNumber={index + 1}
-        width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+        // width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+        width={800}
         onClick={(event) => {
           console.log("hi", event);
         }}
@@ -130,7 +136,6 @@ function CurrentPage({ index, segments }: { index: number; segments: any }) {
             color = "border-black";
           }
 
-          const [hovered, setHovered] = useState(false);
           return (
             <div
               className="page visible absolute z-50 border-2"
