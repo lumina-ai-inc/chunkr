d66ad86ec63f6981890358c3a52adecd505817f9
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-08-30 16:41:39 -0700

    4 bit quantize - new model

--- services/phi35/app/main.py
+++ services/phi35/app/main.py
@@ -9,7 +9,7 @@ import requests
 
 app = FastAPI()
 
-model_id = "microsoft/Phi-3.5-vision-instruct"
+model_id = "yifeihu/TB-OCR-preview-0.1"
 
 # Note: set _attn_implementation='eager' if you don't have flash_attn installed
 model = AutoModelForCausalLM.from_pretrained(
