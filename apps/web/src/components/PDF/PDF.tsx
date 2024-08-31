dba894f2f3e1a005a65f3969930ef8dc04048037
Mehul Chadda <mehul@lumina.sh>
2024-08-31 01:52:34 -0700

    Added hover for type on segments in pdf - starting on click to go to segment

--- apps/web/src/components/PDF/PDF.tsx
+++ apps/web/src/components/PDF/PDF.tsx
@@ -3,7 +3,8 @@ import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import { ScrollArea } from "@radix-ui/themes";
-import { Chunk, Segment } from "../../models/chunk.model";
+import { Chunk, Segment, SegmentType } from "../../models/chunk.model";
+import "./Pdf.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
@@ -12,31 +13,32 @@ const options = {
   standardFontDataUrl: "/standard_fonts/",
 };
 
-type SegmentType =
-  | "Text"
-  | "Title"
-  | "Table"
-  | "Section header"
-  | "Picture"
-  | "Page footer"
-  | "Page header"
-  | "List item"
-  | "Formula"
-  | "Footnote"
-  | "Caption";
-
 const segmentColors: Record<SegmentType, string> = {
   Text: "--teal-10",
   Table: "--orange-9",
-  Title: "--amber-8",
+  Title: "--blue-9",
   Picture: "--pink-10",
-  Formula: "--cyan-8",
-  Caption: "--jade-10",
+  Formula: "--amber-8",
+  Caption: "--crimson-8",
   Footnote: "--pink-10",
   "List item": "--bronze-10",
   "Page footer": "--red-12",
-  "Page header": "--violet-8",
-  "Section header": "--yellow-7",
+  "Page header": "--violet-9",
+  "Section header": "--cyan-8",
+};
+
+const segmentLightColors: Record<SegmentType, string> = {
+  Text: "--teal-4",
+  Table: "--orange-4",
+  Title: "--blue-4",
+  Picture: "--pink-4",
+  Formula: "--amber-4",
+  Caption: "--crimson-2",
+  Footnote: "--pink-4",
+  "List item": "--bronze-4",
+  "Page footer": "--red-4",
+  "Page header": "--violet-4",
+  "Section header": "--cyan-2",
 };
 
 export function PDF({
@@ -109,20 +111,34 @@ function SegmentOverlay({ segment }: { segment: Segment }) {
   const scaledHeight = `${(segment.height / segment.page_height) * 100}%`;
   const scaledWidth = `${(segment.width / segment.page_width) * 100}%`;
 
-  const color = segmentColors[segment.type as SegmentType] || "border-black";
+  const baseColor =
+    segmentColors[segment.type as SegmentType] || "--border-black";
+  const lightColor =
+    segmentLightColors[segment.type as SegmentType] || "--border-black";
 
   return (
     <div
-      className="page visible absolute z-50 border-2"
+      className="segment visible absolute z-50 border-2"
       style={{
         width: scaledWidth,
         height: scaledHeight,
         left: scaledLeft,
         top: scaledTop,
-        borderColor: `var(${color})`,
+        borderColor: `var(${baseColor})`,
       }}
     >
       <div className="w-full h-full bg-red-500 hidden"></div>
+      <div
+        className="segment-overlay"
+        style={{
+          border: `2px solid var(${baseColor})`,
+          backgroundColor: `var(${lightColor})`,
+          color: `var(${baseColor})`,
+          fontSize: "12px",
+        }}
+      >
+        {segment.type}
+      </div>
     </div>
   );
 }
