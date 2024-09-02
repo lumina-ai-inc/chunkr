35994143873d81f2f1f3871f22907867ff500387
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-02 18:21:43 -0400

    working on it

--- chunkmydocs/src/routes/auth.rs
+++ chunkmydocs/src/routes/auth.rs
@@ -1,38 +1,43 @@
-use crate::models::auth::api_key::{ApiKey, ApiKeyLimit, ApiKeyUsage, ApiRequest, ServiceType};
-use crate::utils::db::deadpool_postgres::{Client, Pool};
-use actix_web::{web, Error, HttpResponse};
+use crate::models::auth::auth::UserInfo;
+use crate::utils::db::deadpool_postgres::{ Client, Pool };
+use actix_web::{ web, HttpResponse };
 use prefixed_api_key::PrefixedApiKeyController;
 
 use chrono::Utc;
 
-pub async fn create_api_key(
-    request_payload: web::Json<ApiRequest>,
-    pool: web::Data<Pool>,
+/// Get User
+///
+/// Get user information, if its the first time the user is logging in, create a new user
+#[utoipa::path(
+    get,
+    path = "/user",
+    context_path = "",
+    tag = "user",
+    responses((
+        status = 200,
+        description = "Get user information, if it's the first time the user is logging in, create a new user",
+        body = UserResponse,
+    ))
+)]
+pub async fn get_or_create_user(
+    user_info: web::ReqData<UserInfo>,
+    pool: web::Data<Pool>
 ) -> Result<HttpResponse, Error> {
-    let request = request_payload.into_inner();
-    match create_api_key_query(request, &pool).await {
-        Ok(new_key) => Ok(HttpResponse::Ok().json(new_key)),
-        Err(e) => {
-            println!("Error creating API key: {:?}", e);
-            Ok(HttpResponse::InternalServerError().json(format!("Error: {:?}", e)))
-        }
-    }
+    
 }
 
 pub async fn create_api_key_query(
     request: ApiRequest,
-    pool: &Pool,
+    pool: &Pool
 ) -> Result<String, Box<dyn std::error::Error>> {
     let mut pg_client: Client = pool.get().await?;
 
-    let existing_key_query = r#"
+    let existing_key_query =
+        r#"
     SELECT key FROM api_users WHERE email = $1 AND key IS NOT NULL AND key != ''
     "#;
 
-    if let Some(row) = pg_client
-        .query_opt(existing_key_query, &[&request.email])
-        .await?
-    {
+    if let Some(row) = pg_client.query_opt(existing_key_query, &[&request.email]).await? {
         let existing_key: String = row.get(0);
         return Ok(existing_key);
     }
@@ -82,85 +87,81 @@ pub async fn create_api_key_query(
 
     let transaction = pg_client.transaction().await?;
 
-    let insert_api_key = r#"
+    let insert_api_key =
+        r#"
     INSERT INTO api_keys (key, user_id, dataset_id, org_id, access_level, active, deleted, created_at, expires_at, deleted_at, deleted_by)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
     "#;
 
-    let insert_api_key_usage = r#"
+    let insert_api_key_usage =
+        r#"
     INSERT INTO api_key_usage (api_key, usage, usage_type, created_at, service)
     VALUES ($1, $2, $3, $4, $5)
     "#;
 
-    let insert_api_key_limit = r#"
+    let insert_api_key_limit =
+        r#"
     INSERT INTO api_key_limit (api_key, usage_limit, usage_type, created_at, service)
     VALUES ($1, $2, $3, $4, $5)
     "#;
 
-    let insert_user = r#"
+    let insert_user =
+        r#"
     INSERT INTO api_users (key, user_id, email, created_at, usage_type, usage_limit, service, usage)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     "#;
 
-    transaction
-        .execute(
-            insert_api_key,
-            &[
-                &api_key.key,
-                &api_key.user_id,
-                &api_key.dataset_id,
-                &api_key.org_id,
-                &api_key.access_level.as_ref().map(|al| al.to_string()),
-                &api_key.active,
-                &api_key.deleted,
-                &api_key.created_at,
-                &api_key.expires_at,
-                &api_key.deleted_at,
-                &api_key.deleted_by,
-            ],
-        )
-        .await?;
+    transaction.execute(
+        insert_api_key,
+        &[
+            &api_key.key,
+            &api_key.user_id,
+            &api_key.dataset_id,
+            &api_key.org_id,
+            &api_key.access_level.as_ref().map(|al| al.to_string()),
+            &api_key.active,
+            &api_key.deleted,
+            &api_key.created_at,
+            &api_key.expires_at,
+            &api_key.deleted_at,
+            &api_key.deleted_by,
+        ]
+    ).await?;
 
-    transaction
-        .execute(
-            insert_api_key_usage,
-            &[
-                &api_key_usage.api_key,
-                &api_key_usage.usage,
-                &api_key_usage.usage_type.as_ref().map(|ut| ut.to_string()),
-                &api_key_usage.created_at,
-                &api_key_usage.service_type.as_ref().map(|st| st.to_string()),
-            ],
-        )
-        .await?;
+    transaction.execute(
+        insert_api_key_usage,
+        &[
+            &api_key_usage.api_key,
+            &api_key_usage.usage,
+            &api_key_usage.usage_type.as_ref().map(|ut| ut.to_string()),
+            &api_key_usage.created_at,
+            &api_key_usage.service_type.as_ref().map(|st| st.to_string()),
+        ]
+    ).await?;
 
-    transaction
-        .execute(
-            insert_api_key_limit,
-            &[
-                &api_key_limit.api_key,
-                &api_key_limit.usage_limit,
-                &api_key_limit.usage_type.as_ref().map(|ut| ut.to_string()),
-                &api_key_limit.created_at,
-                &api_key_limit.service_type.as_ref().map(|st| st.to_string()),
-            ],
-        )
-        .await?;
-    transaction
-        .execute(
-            insert_user,
-            &[
-                &api_key.key,
-                &api_key.user_id,
-                &email,
-                &api_key.created_at,
-                &api_key_usage.usage_type.as_ref().map(|ut| ut.to_string()),
-                &api_key_limit.usage_limit,
-                &api_key_usage.service_type.as_ref().map(|st| st.to_string()),
-                &api_key_usage.usage,
-            ],
-        )
-        .await?;
+    transaction.execute(
+        insert_api_key_limit,
+        &[
+            &api_key_limit.api_key,
+            &api_key_limit.usage_limit,
+            &api_key_limit.usage_type.as_ref().map(|ut| ut.to_string()),
+            &api_key_limit.created_at,
+            &api_key_limit.service_type.as_ref().map(|st| st.to_string()),
+        ]
+    ).await?;
+    transaction.execute(
+        insert_user,
+        &[
+            &api_key.key,
+            &api_key.user_id,
+            &email,
+            &api_key.created_at,
+            &api_key_usage.usage_type.as_ref().map(|ut| ut.to_string()),
+            &api_key_limit.usage_limit,
+            &api_key_usage.service_type.as_ref().map(|st| st.to_string()),
+            &api_key_usage.usage,
+        ]
+    ).await?;
 
     transaction.commit().await?;
 
