e2780fab397ac67ddcb5b3f51785426be7d62ffc
Mehul Chadda <mehul@lumina.sh>
2024-09-04 15:46:29 -0700

    Updates

--- apps/web/src/auth/Auth.tsx
+++ apps/web/src/auth/Auth.tsx
@@ -3,16 +3,19 @@ import { useAuth } from "react-oidc-context";
 import axiosInstance from "../services/axios.config";
 import { getUser } from "../services/user";
 
-export default function Auth({children}: {children: ReactNode}  ) {
+export default function Auth({ children }: { children: ReactNode }) {
   const auth = useAuth();
 
   useEffect(() => {
     console.log(auth.user);
     if (auth.isAuthenticated) {
-      axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${auth.user?.access_token}`;
-      getUser().then().catch((error) => {
-        console.log("error:", error);
-      });
+      axiosInstance.defaults.headers.common["Authorization"] =
+        `Bearer ${auth.user?.access_token}`;
+      getUser()
+        .then()
+        .catch((error) => {
+          console.log("error:", error);
+        });
     }
   }, [auth.isAuthenticated, auth.user]);
 
