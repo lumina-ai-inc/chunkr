a6b30ceaae067c6557f661459074b424d301e8ba
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-09-06 10:22:36 -0700

    api

--- pyscripts/api.py
+++ pyscripts/api.py
@@ -3,29 +3,25 @@ import os
 import requests
 from models import Model, TaskResponse
 import time
+import glob
 
 dotenv.load_dotenv(override=True)
-
 
 def get_base_url():
     return os.getenv("INGEST_SERVER__URL")
 
-
-def get_api_key():
-    return os.getenv("INGEST_SERVER__API_KEY")
-
+def get_access_token():
+    return os.getenv("INGEST_SERVER__ACCESS_TOKEN")
 
 def get_headers():
     api_key = get_api_key()
     headers = {"Authorization": api_key}
     return headers
 
-
 def health_check():
     url = get_base_url() + "/health"
     response = requests.get(url, headers=get_headers()).raise_for_status()
     return response
-
 
 def extract_file(file_to_send, model: Model) -> TaskResponse:
     url = get_base_url() + "/api/task"
@@ -35,8 +31,11 @@ def extract_file(file_to_send, model: Model) -> TaskResponse:
         headers = get_headers()
         response = requests.post(url, files=file, data=file_data, headers=headers)
         response.raise_for_status()
-        return TaskResponse(**response.json())
-
+        response_data = response.json()
+        # Ensure configuration is included in the response
+        if 'configuration' not in response_data:
+            raise ValueError("Missing 'configuration' in response")
+        return TaskResponse(**response_data)
 
 def get_task(url: str) -> TaskResponse:
     headers = get_headers()
@@ -44,7 +43,6 @@ def get_task(url: str) -> TaskResponse:
     response.raise_for_status()
     task = TaskResponse(**response.json())
     return task
-
 
 def check_task_status(url: str) -> TaskResponse:
     task = get_task(url)
@@ -55,18 +53,25 @@ def check_task_status(url: str) -> TaskResponse:
         raise Exception(task.message)
     return task
 
-
 def process_file(file_path: str, model: Model) -> TaskResponse:
     health_check()
     task = extract_file(file_path, model)
     task = check_task_status(task.task_url)
     return task
 
+def process_all_files_in_input_folder(model: Model):
+    input_folder = "input"
+    pdf_files = glob.glob(os.path.join(input_folder, "*.pdf"))
+    
+    for file_path in pdf_files:
+        print(f"Processing file: {file_path}")
+        try:
+            task = process_file(file_path, model)
+            print(f"Task completed for {file_path}:")
+            print(task)
+        except Exception as e:
+            print(f"Error processing {file_path}: {str(e)}")
 
 if __name__ == "__main__":
-    # file_path = r"/Users/akhileshsharma/Documents/Lumina/backend/example-scripts/Extraction/input/apple_file_stamped_complaint_3.21.24.pdf"
-    file_path = r"/Users/ishaankapoor/chunk-my-docs/pyscripts/input/test.pdf"
-
-    model = Model.HighQuality
-    task = process_file(file_path, model)
-    print(task)
+    model = Model.Fast
+    process_all_files_in_input_folder(model)
