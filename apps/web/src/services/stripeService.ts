76707b7e479f7fbc7b9c463eb1d1676d7037df55
Mehul Chadda <mehul@lumina.sh>
2024-09-06 18:36:06 -0700

    Updates

--- apps/web/src/services/stripeService.ts
+++ apps/web/src/services/stripeService.ts
@@ -2,11 +2,14 @@ import axiosInstance from "./axios.config";
 
 export async function createSetupIntent(accessToken: string) {
   try {
-    const response = await axiosInstance.get("/stripe/create-setup-intent", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    const response = await axiosInstance.get(
+      "/api/stripe/create-setup-intent",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
     return response.data.clientSecret; // Return the client secret directly
   } catch (error) {
     console.error("Error creating setup intent:", error);
