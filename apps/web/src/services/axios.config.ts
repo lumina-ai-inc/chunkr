307258572f9ba1250e4d034410915b2cd8601e11
Mehul Chadda <mehul@lumina.sh>
2024-09-06 18:36:06 -0700

    Updates

--- apps/web/src/services/axios.config.ts
+++ apps/web/src/services/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default axiosInstance;
