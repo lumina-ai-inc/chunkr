d009a7a13f4a6ac0ea8cd42fd568a9a96f45edc1
Mehul Chadda <mehul@lumina.sh>
2024-09-07 19:20:47 -0700

    Flipped

--- apps/web/src/pages/Home/Home.tsx
+++ apps/web/src/pages/Home/Home.tsx
@@ -21,180 +21,141 @@ const Home = () => {
       className="pulsing-background"
     >
       <ScrollArea>
-        <div
-          style={{
-            maxWidth: "1564px",
-            margin: "0 auto",
-            marginBottom: "152px",
-          }}
-        >
-          <Header px="0px" home={true} />
-          <Flex className="hero-container">
-            <Flex className="text-container" direction="column">
-              <Text size="9" weight="medium" trim="both" className="hero-title">
-                Open Source Data Ingestion for LLMs & RAG
-              </Text>
-              <Text
-                className="cyan-2"
-                size="5"
-                weight="medium"
+        <div>
+          <div className="hero-main-container">
+            <div className="hero-image-container">
+              <img
+                src="src/assets/hero-image.png"
+                alt="hero"
+                className="hero-image"
                 style={{
-                  maxWidth: "542px",
-                  lineHeight: "32px",
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
                 }}
-              >
-                Small tagline. Epxlain what demo does and what all can actually
-                be done with API/self-hosting.
-              </Text>
-              <Flex
-                direction="row"
-                align="start"
-                className="credit-main-container"
-              >
-                <Flex
-                  className="credit-container"
-                  direction="column"
-                  gap="16px"
-                  mt="40px"
-                >
-                  <Text size="6" weight="bold" className="cyan-3">
-                    Powered by
+              />
+              <div className="hero-gradient-overlay"></div>
+            </div>
+            <div className="hero-content-container">
+              <Header px="0px" home={true} />
+              <Flex className="hero-container">
+                <Flex className="text-container" direction="column">
+                  <Text
+                    size="9"
+                    weight="bold"
+                    trim="both"
+                    className="hero-title"
+                  >
+                    Open Source Data Ingestion for LLMs & RAG
                   </Text>
-                  <Flex direction="row" gap="32px" align="center">
-                    <a
-                      href="https://www.lumina.sh/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Flex
-                        gap="12px"
-                        align="center"
-                        justify="center"
-                        className="credit-button"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="32"
-                          height="32"
-                          viewBox="0 0 32 32"
-                          fill="none"
-                        >
-                          <circle cx="16" cy="16" r="16" fill="#D1F0FA" />
-                          <path
-                            d="M9.57141 8.28572H22.4286M10.8571 8.28572V22C10.8571 22.4547 11.0377 22.8907 11.3592 23.2122C11.6807 23.5337 12.1168 23.7143 12.5714 23.7143H19.4286C19.8832 23.7143 20.3192 23.5337 20.6407 23.2122C20.9622 22.8907 21.1428 22.4547 21.1428 22V8.28572M10.8571 17.7143H21.1428"
-                            stroke="#60B3D7"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          />
-                        </svg>
-                        <Text size="7" weight="bold" className="cyan-1">
-                          Lumina
-                        </Text>
-                      </Flex>
-                    </a>
-                  </Flex>
-                </Flex>
-                <Flex
-                  className="credit-container"
-                  direction="column"
-                  gap="16px"
-                  mt="40px"
-                >
-                  <Text size="6" weight="bold" className="cyan-3">
-                    Core Contributors
-                  </Text>
-                  <Flex
-                    direction="row"
-                    gap="32px"
-                    align="center"
+                  <Text
+                    className="white"
+                    size="5"
+                    weight="medium"
                     style={{
-                      padding: "12px 18px",
-                      borderRadius: "8px",
-                      border: "1px solid var(--cyan-4)",
-                      backgroundColor: "#061d22",
-                      height: "62px",
+                      maxWidth: "542px",
+                      lineHeight: "32px",
                     }}
                   >
-                    <Flex direction="row" gap="16px">
-                      <a
-                        href="https://github.com/skeptrunedev"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                    Small tagline. Epxlain what demo does and what all can
+                    actually be done with API/self-hosting.
+                  </Text>
+                  <Flex
+                    className="signup-container"
+                    direction="column"
+                    gap="16px"
+                  >
+                    <Flex
+                      direction="row"
+                      align="center"
+                      className="signup-button"
+                    >
+                      <Text size="3" weight="bold" className="cyan-12">
+                        Get started for free
+                      </Text>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
                       >
-                        <Flex height="32px" width="32px" className="credit-dp">
-                          <img
-                            src="src/assets/Nick.jpeg"
-                            alt="Nick from Trieve"
-                            style={{ borderRadius: "50%" }}
-                          />
-                        </Flex>
-                      </a>
-                      <a
-                        href="https://github.com/cdxker"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Flex height="32px" width="32px" className="credit-dp">
-                          <img
-                            src="src/assets/Denzell.png"
-                            alt="Denzell from Trieve"
-                            style={{ borderRadius: "50%" }}
-                          />
-                        </Flex>
-                      </a>
-                      <a
-                        href="https://github.com/akhileshsharma99"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Flex height="32px" width="32px" className="credit-dp">
-                          <img
-                            src="src/assets/Akhilesh.jpeg"
-                            alt="Akhilesh from Lumina"
-                            style={{ borderRadius: "50%" }}
-                          />
-                        </Flex>
-                      </a>
-                      <a
-                        href="https://github.com/m-chadda"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Flex height="32px" width="32px" className="credit-dp">
-                          <img
-                            src="src/assets/Mehul.jpeg"
-                            alt="Mehul from Lumina"
-                            style={{ borderRadius: "50%" }}
-                          />
-                        </Flex>
-                      </a>
-                      <a
-                        href="https://github.com/ishaan99k"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Flex height="32px" width="32px" className="credit-dp">
-                          <img
-                            src="src/assets/Ishaan.png"
-                            alt="Ishaan from Lumina"
-                            style={{ borderRadius: "50%" }}
-                          />
-                        </Flex>
-                      </a>
+                        <rect
+                          width="24"
+                          height="24"
+                          fill="white"
+                          fill-opacity="0.01"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M13.0343 5.03431C13.3467 4.72188 13.8532 4.72188 14.1656 5.03431L20.5657 11.4343C20.878 11.7467 20.878 12.2533 20.5657 12.5657L14.1656 18.9658C13.8532 19.2781 13.3467 19.2781 13.0343 18.9658C12.7218 18.6533 12.7218 18.1467 13.0343 17.8342L18.0686 12.8H3.99995C3.55813 12.8 3.19995 12.4418 3.19995 12C3.19995 11.5582 3.55813 11.2 3.99995 11.2H18.0686L13.0343 6.16567C12.7218 5.85326 12.7218 5.34673 13.0343 5.03431Z"
+                          fill="#0D3C48"
+                        />
+                      </svg>
+                    </Flex>
+                  </Flex>
+                  <Flex
+                    direction="column"
+                    align="start"
+                    className="credit-main-container"
+                  >
+                    <Flex
+                      className="credit-container"
+                      direction="column"
+                      gap="16px"
+                    >
+                      <Text size="6" weight="medium" className="white">
+                        Powered by
+                      </Text>
+                      <Flex direction="row" gap="32px" align="center">
+                        <a
+                          href="https://www.lumina.sh/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <Flex
+                            gap="12px"
+                            align="center"
+                            justify="center"
+                            className="credit-button"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="32"
+                              height="32"
+                              viewBox="0 0 32 32"
+                              fill="none"
+                            >
+                              <circle cx="16" cy="16" r="16" fill="#D1F0FA" />
+                              <path
+                                d="M9.57141 8.28572H22.4286M10.8571 8.28572V22C10.8571 22.4547 11.0377 22.8907 11.3592 23.2122C11.6807 23.5337 12.1168 23.7143 12.5714 23.7143H19.4286C19.8832 23.7143 20.3192 23.5337 20.6407 23.2122C20.9622 22.8907 21.1428 22.4547 21.1428 22V8.28572M10.8571 17.7143H21.1428"
+                                stroke="#60B3D7"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              />
+                            </svg>
+                            <Text size="7" weight="bold" className="cyan-1">
+                              Lumina
+                            </Text>
+                          </Flex>
+                        </a>
+                      </Flex>
                     </Flex>
                   </Flex>
                 </Flex>
+                <Flex className="module-container" direction="column" gap="4">
+                  <UploadMain isAuthenticated={isAuthenticated} />
+                </Flex>
               </Flex>
-            </Flex>
-            <Flex className="module-container" direction="column" gap="4">
-              <UploadMain isAuthenticated={isAuthenticated} />
-            </Flex>
-          </Flex>
-          <Flex p="80px">
+            </div>
+          </div>
+          <Flex p="80px" mt="40px">
             <ModelTable />
           </Flex>
         </div>
+
         <Footer />
       </ScrollArea>
     </Flex>
