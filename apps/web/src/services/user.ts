bfcefc367a11b2c2c1363ea065030b1ab056d241
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-10 13:09:47 -0700

    fixed ts

--- apps/web/src/services/user.ts
+++ apps/web/src/services/user.ts
@@ -2,6 +2,7 @@ import axiosInstance from "./axios.config";
 import { AppDispatch } from "../store/store";
 import { setUserData, setUserLoading, setUserError } from "../store/userSlice";
 import { User } from "../models/user.model";
+import { TaskResponse } from "../models/task.model";
 
 export async function getUser(dispatch: AppDispatch): Promise<void> {
   dispatch(setUserLoading(true));
@@ -17,7 +18,7 @@ export async function getUser(dispatch: AppDispatch): Promise<void> {
 
 export async function getTasks(page: number, limit: number): Promise<void> {
   try {
-    const { data } = await axiosInstance.get<User>("/api/tasks?page=" + page + "&limit=" + limit);
+    const { data } = await axiosInstance.get<TaskResponse[]>("/api/tasks?page=" + page + "&limit=" + limit);
     console.log("tasks:", data);
   } catch (error) {
     console.error("Error fetching user data:", error);
