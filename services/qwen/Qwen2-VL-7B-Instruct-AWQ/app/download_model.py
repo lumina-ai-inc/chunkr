1f21a67e210484d9b512aebf448db997479dc3ba
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-09-10 15:52:11 -0700

    added Qwen2-VL-7B-Instruct-AWQ support

---
+++ services/qwen/Qwen2-VL-7B-Instruct-AWQ/app/download_model.py
@@ -0,0 +1,16 @@
+
+from huggingface_hub import snapshot_download
+import os
+# access_token = "<YOUR-HUGGINGFACE_TOKEN>"
+
+
+
+if __name__ == "__main__":
+    # download the meta/llama3 model
+    os.makedirs("./models", exist_ok=True)
+    snapshot_download(
+        repo_id="Qwen/Qwen2-VL-7B-Instruct-AWQ",
+        local_dir="models",
+        ignore_patterns=["*.pt", "*.bin"],
+        #    token=access_token,
+    )
