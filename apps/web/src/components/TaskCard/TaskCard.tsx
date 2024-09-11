c453034cdb449c2b3e283807a14509f110c1b313
Mehul Chadda <mehul@lumina.sh>
2024-09-10 18:12:09 -0700

    Updates

--- apps/web/src/components/TaskCard/TaskCard.tsx
+++ apps/web/src/components/TaskCard/TaskCard.tsx
@@ -1,12 +1,22 @@
-import { Flex, Text } from "@radix-ui/themes";
+import { Flex, Text, Badge } from "@radix-ui/themes";
 import "./Taskcard.css";
-import { TaskResponse } from "../../models/task.model";
+import { TaskResponse, Status } from "../../models/task.model";
 
 export interface TaskCardProps extends TaskResponse {
   onClick?: () => void;
 }
 
+const statusColors = {
+  [Status.Starting]: "cyan-4",
+  [Status.Processing]: "cyan-8",
+  [Status.Succeeded]: "cyan-11",
+  [Status.Failed]: "red-8",
+  [Status.Canceled]: "amber-8",
+};
+
 export default function TaskCard({ onClick, ...task }: TaskCardProps) {
+  const statusColor = statusColors[task.status];
+
   return (
     <Flex
       direction="row"
@@ -15,17 +25,31 @@ export default function TaskCard({ onClick, ...task }: TaskCardProps) {
       className="task-card"
       onClick={onClick}
     >
-      <Flex direction="row" align="center" justify="between">
-        <Text size="2" weight="bold" style={{ color: "var(--cyan-3)" }}>
-          {new Date(task.created_at).toLocaleString("en-US", {
-            month: "short",
-            day: "numeric",
-            hour: "numeric",
-            minute: "2-digit",
-            hour12: true,
-          })}
-        </Text>
-      </Flex>
+      <Text size="2" weight="bold" className="cyan-3">
+        {new Date(task.created_at).toLocaleString("en-US", {
+          month: "short",
+          day: "numeric",
+          hour: "numeric",
+          minute: "2-digit",
+          hour12: true,
+        })}
+      </Text>
+      <Text
+        size="2"
+        className="cyan-2"
+        style={{ flex: 1, marginLeft: "16px", marginRight: "16px" }}
+      >
+        {task.message}
+      </Text>
+      <Text size="2" className="cyan-4" style={{ marginRight: "16px" }}>
+        {task.configuration.model}
+      </Text>
+      <Text size="2" className="cyan-4" style={{ marginRight: "16px" }}>
+        {task.configuration.target_chunk_length || "N/A"}
+      </Text>
+      <Badge size="1" style={{ backgroundColor: statusColor }}>
+        {task.status}
+      </Badge>
     </Flex>
   );
 }
