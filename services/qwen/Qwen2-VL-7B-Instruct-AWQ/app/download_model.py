ef4de04724602d91e89bc252d2994daafb86f4a0
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-09-10 18:27:47 -0700

    2B

--- services/qwen/Qwen2-VL-7B-Instruct-AWQ/app/download_model.py
+++ services/qwen/Qwen2-VL-7B-Instruct-AWQ/app/download_model.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # download the meta/llama3 model
     os.makedirs("./models", exist_ok=True)
     snapshot_download(
-        repo_id="Qwen/Qwen2-VL-7B-Instruct-AWQ",
+        repo_id="Qwen/Qwen2-VL-2B-Instruct-AWQ",
         local_dir="models",
         ignore_patterns=["*.pt", "*.bin"],
         #    token=access_token,
