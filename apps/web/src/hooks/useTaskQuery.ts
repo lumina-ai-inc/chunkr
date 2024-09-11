61245197d4ca8308ecd5d1c67f3cca688258a01a
Mehul Chadda <mehul@lumina.sh>
2024-09-10 17:50:18 -0700

    Updates

--- apps/web/src/hooks/useTaskQuery.ts
+++ apps/web/src/hooks/useTaskQuery.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { getTask } from "../services/taskApi";
+import { getTask, getTasks } from "../services/taskApi";
 import { TaskResponse, Status } from "../models/task.model";
 import { useState } from "react";
 
@@ -17,6 +17,16 @@ export function useTaskQuery(taskId: string | undefined) {
       onSuccess: (data) => {
         setTaskResponse(data);
       },
+    }
+  );
+}
+
+export function useTasksQuery(page: number, limit: number) {
+  return useQuery<TaskResponse[], Error>(
+    [`tasks-${page}-${limit}`],
+    () => getTasks(page, limit),
+    {
+      staleTime: 5000,
     }
   );
 }
