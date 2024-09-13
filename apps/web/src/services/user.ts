308743e974e652abed9f72c5d9e0a839efbccd42
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-12 21:14:07 -0700

    fixed user bug

--- apps/web/src/services/user.ts
+++ apps/web/src/services/user.ts
@@ -1,15 +1,7 @@
 import axiosInstance from "./axios.config";
-import { AppDispatch } from "../store/store";
-import { setUserData, setUserLoading, setUserError } from "../store/userSlice";
 import { User } from "../models/user.model";
 
-export async function getUser(dispatch: AppDispatch): Promise<void> {
-  dispatch(setUserLoading(true));
-  try {
-    const { data } = await axiosInstance.get<User>("/api/user");
-    dispatch(setUserData(data));
-  } catch (error) {
-    console.error("Error fetching user data:", error);
-    dispatch(setUserError("Failed to fetch user data"));
-  }
+export async function getUser(): Promise<User> {
+  const { data } = await axiosInstance.get<User>("/api/user");
+  return data;
 }
