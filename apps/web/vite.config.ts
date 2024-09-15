37946b6c6cdffad80f723a31f581f80153b36f50
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-15 09:40:02 -0700

    working on runtime env for vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import runtimeEnv from 'vite-plugin-runtime-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    runtimeEnv(),
+  ],
 });
