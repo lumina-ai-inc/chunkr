a3f19913dcef44eac4ee6a02efcda5bc71dc9932
Mehul Chadda <mehul@lumina.sh>
2024-09-18 13:01:43 -0700

    Fixes for discount usage value and new account loading error for dashboard

--- apps/web/src/components/PriceCalculator/Calculator.tsx
+++ apps/web/src/components/PriceCalculator/Calculator.tsx
@@ -28,23 +28,38 @@ export default function Calculator() {
       }}
     >
       <Flex direction="column" width="100%" p="8">
-        <Text weight="bold" size="4" className="cyan-4" trim="start">
+        <Text
+          weight="bold"
+          size="4"
+          style={{ color: "hsl(0, 0%, 100%, 0.98)" }}
+        >
           API Calculator
         </Text>
 
         <Flex direction="column" mt="6" mb="5" gap="2">
-          <Text size="9" weight="bold" className="cyan-2">
+          <Text
+            size="9"
+            weight="bold"
+            style={{ color: "hsl(0, 0%, 100%, 0.98)" }}
+          >
             ${calculateTotalCost()}
           </Text>
           <Flex direction="row" gap="2" align="end">
-            <Text size="6" weight="medium" className="cyan-8">
+            <Text
+              size="6"
+              weight="medium"
+              style={{ color: "hsl(0, 0%, 100%, 0.9)" }}
+            >
               Estimated cost
             </Text>
             <Text
               size="2"
               weight="light"
-              className="cyan-5"
-              style={{ fontStyle: "italic", lineHeight: "1.5" }}
+              style={{
+                color: "hsl(0, 0%, 100%, 0.6)",
+                fontStyle: "italic",
+                lineHeight: "1.5",
+              }}
             >
               *Billed monthly
             </Text>
@@ -77,8 +92,8 @@ export default function Calculator() {
                       />
                       <path
                         d="M9.31576 0.951108L9.27642 1.04304L9.31576 0.951107C9.04732 0.836241 8.73514 0.919785 8.55994 1.15337L2.15996 9.68667L2.15996 9.68667C2.01603 9.87858 1.99289 10.1353 2.10016 10.3499L2.10017 10.3499C2.20745 10.5645 2.42675 10.7 2.66663 10.7H7.25232L6.30857 16.3625C6.26056 16.6506 6.41569 16.9341 6.68413 17.0489L6.68417 17.049C6.95257 17.1637 7.26475 17.0803 7.43994 16.8467L13.8399 8.31337C13.8399 8.31336 13.8399 8.31335 13.8399 8.31334C13.9839 8.12143 14.0071 7.86468 13.8998 7.65012C13.7925 7.43554 13.5731 7.30001 13.3333 7.30001H8.74757L9.69132 1.63749C9.73933 1.34948 9.5842 1.06597 9.31576 0.951108ZM7.5167 8.34272L7.5167 8.34272C7.63704 8.48476 7.81378 8.56668 7.99994 8.56668H12.0666L7.98499 14.0088L8.62466 10.1708C8.65527 9.98716 8.60352 9.79935 8.48319 9.6573C8.36286 9.51525 8.18611 9.43334 7.99994 9.43334H3.9333L8.01491 3.99119L7.37524 7.82922C7.37524 7.82922 7.37524 7.82922 7.37524 7.82922C7.34462 8.01287 7.39637 8.20066 7.5167 8.34272Z"
-                        fill="#0D3C48"
-                        stroke="#0D3C48"
+                        fill="#000"
+                        stroke="#000"
                         stroke-width="0.2"
                       />
                     </svg>
@@ -206,22 +221,35 @@ function ExplanationSection({
         py="4px"
         style={{
           borderRadius: "99px",
-          border: "2px solid var(--cyan-2)",
-          backgroundColor: "var(--cyan-1)",
+          border: "2px solid hsla(180, 100%, 100%, 0.15)",
+          backgroundColor: "hsla(180, 100%, 100%, 0.1)",
         }}
       >
         {icon}
-        <Text size="2" weight="bold" className="cyan-12">
+        <Text
+          size="2"
+          weight="bold"
+          style={{ color: "hsl(0, 0%, 100%, 0.98)" }}
+        >
           {title}
         </Text>
       </Flex>
-      <Text size="7" weight="bold" className="cyan-1">
+      <Text size="7" weight="bold" style={{ color: "hsl(0, 0%, 100%, 0.98)" }}>
         ${price}{" "}
-        <Text size="3" weight="regular" className="cyan-5">
+        <Text
+          size="3"
+          weight="regular"
+          style={{ color: "hsl(0, 0%, 100%, 0.6)" }}
+        >
           {free}
         </Text>
       </Text>
-      <Text size="4" weight="regular" className="cyan-2" trim="both">
+      <Text
+        size="4"
+        weight="regular"
+        style={{ color: "hsl(0, 0%, 100%, 0.9)" }}
+        trim="both"
+      >
         {description}
       </Text>
     </Flex>
