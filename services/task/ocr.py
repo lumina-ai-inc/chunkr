c6e144b8c6dd2ad78ccfd1bf01d90edca93fd592
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-18 11:53:50 -0700

    lfg

--- services/task/ocr.py
+++ services/task/ocr.py
@@ -4,11 +4,8 @@ from pathlib import Path
 def perform_paddle_ocr(ocr: PaddleOCR, image_path: Path) -> list:
     try:
         image_path = str(image_path)
-        print("OCR image path: ", image_path)
         result = ocr.ocr(image_path)
-        print(result)
     except Exception as e:
         result = []
         print(f"An error occurred: {str(e)}")
-
     return result
