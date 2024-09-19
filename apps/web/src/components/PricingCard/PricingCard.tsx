7a10bf6e78bd0094347fa1572f7f40da2d3ee658
Mehul Chadda <mehul@lumina.sh>
2024-09-19 15:25:08 -0700

    COmpleted pricing page copy

--- apps/web/src/components/PricingCard/PricingCard.tsx
+++ apps/web/src/components/PricingCard/PricingCard.tsx
@@ -4,42 +4,62 @@ import "./PricingCard.css";
 
 // Add this component at the end of the file
 interface PricingCardProps {
-  tier: string;
   title: string;
-  text: string;
+  checkpoints?: string[];
+  icon: React.ReactNode;
 }
 
-export default function PricingCard({ tier, title, text }: PricingCardProps) {
+export default function PricingCard({
+  title,
+  checkpoints,
+  icon,
+}: PricingCardProps) {
   return (
     <Flex direction="column" width="100%" className="pricing-card-container">
-      <Text
-        size="4"
-        weight="medium"
-        style={{ color: "hsla(180, 100%, 100%, 0.92)" }}
-        trim="start"
+      <Flex
+        direction="row"
+        gap="4"
+        style={{
+          padding: "10px 12px",
+          borderRadius: "8px",
+          backgroundColor: "hsla(180, 100%, 100%, 0.1)",
+          width: "fit-content",
+        }}
       >
-        {tier}
-      </Text>
-      <Text
-        size="8"
-        weight="bold"
-        style={{ marginTop: "16px", color: "white" }}
-      >
-        {title}
-      </Text>
-
-      <Flex direction="column" gap="20px" mt="4">
-        <Text
-          size="4"
-          weight="regular"
-          style={{ color: "hsla(180, 100%, 100%, 0.95)" }}
-        >
-          {text}
+        {icon}
+        <Text size="4" weight="bold" style={{ color: "white" }}>
+          {title}
         </Text>
       </Flex>
-      <Flex mt="32px">
+
+      <Flex direction="column" gap="20px" mt="5">
+        {checkpoints && (
+          <Flex className="checkpoints-container">
+            {checkpoints.map((checkpoint, index) => (
+              <Flex
+                key={index}
+                align="center"
+                gap="8px"
+                className="checkpoint-item"
+              >
+                <Text size="3" style={{ color: "hsla(180, 100%, 100%, 0.95)" }}>
+                  ✓
+                </Text>
+                <Text
+                  size="3"
+                  weight="medium"
+                  style={{ color: "hsla(180, 100%, 100%, 0.95)" }}
+                >
+                  {checkpoint}
+                </Text>
+              </Flex>
+            ))}
+          </Flex>
+        )}
+      </Flex>
+      <Flex mt="44px">
         <BetterButton>
-          <Text size="2" weight="medium" className="white">
+          <Text size="3" weight="medium" className="white">
             Book a Call
           </Text>
         </BetterButton>
