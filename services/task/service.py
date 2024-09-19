f3981ae30efba1a4cbd0d0d4a0e8a62f78d6eeee
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-19 11:59:11 -0700

    visualizing tabe ocr

--- services/task/service.py
+++ services/task/service.py
@@ -52,7 +52,7 @@ class OCR:
         self.ocr = PaddleOCR(use_angle_cls=True, lang="en",
                              ocr_order_method="tb-xy")
         self.table_engine = PPStructure(
-            recovery=True, return_ocr_result_in_table=True, show_log=True)
+            recovery=True, return_ocr_result_in_table=True, type="structure")
 
     @bentoml.api
     def paddle_raw(self, file: Path) -> list:
