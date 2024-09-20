c0b47c4dddd34295ea9479ec94c92abe1cf277bc
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-20 11:28:48 -0700

    math mistake

--- services/task/src/process.py
+++ services/task/src/process.py
@@ -2,7 +2,7 @@ from src.models.segment_model import Segment
 
 def adjust_segments(segments: list[Segment], offset: float = 5.0):
     for segment in segments:
-        segment.width = segment.width + offset
-        segment.height = segment.height + offset
+        segment.width = segment.width + offset * 2
+        segment.height = segment.height + offset * 2
         segment.left = segment.left - offset
         segment.top = segment.top - offset
