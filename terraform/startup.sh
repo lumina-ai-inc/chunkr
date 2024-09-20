aace281a6e7452c55acfa3b37ded0cd0d1522f5b
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-20 13:22:06 -0700

    added image magick to startup.sh

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -1,5 +1,35 @@
 apt-get update
-apt-get install -y redis-tools htop git build-essential dkms
+apt-get install -y redis-tools htop git inux-headers-`uname -r` build-essential dkms
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.cargo/env
+
+# Install ImageMagick dependencies
+apt-get install -y wget autoconf pkg-config build-essential curl libpng-dev ghostscript libgs-dev libpdf-dev
+
+# Download and install ImageMagick
+wget https://github.com/ImageMagick/ImageMagick/archive/refs/tags/7.1.0-38.tar.gz
+tar xzf 7.1.0-38.tar.gz
+rm 7.1.0-38.tar.gz
+cd ImageMagick-7.1.0-38
+./configure --prefix=/usr/local \
+    --with-bzlib=yes \
+    --with-fontconfig=yes \
+    --with-freetype=yes \
+    --with-gslib=yes \
+    --with-gvc=yes \
+    --with-jpeg=yes \
+    --with-jp2=yes \
+    --with-png=yes \
+    --with-tiff=yes \
+    --with-xml=yes \
+    --with-gs-font-dir=yes \
+    --with-gslib=yes \
+    --with-pdf=yes
+make -j
+make install
+ldconfig /usr/local/lib
+cd ..
+rm -rf ImageMagick-7.1.0-38
 
 # Install Docker
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
@@ -13,3 +43,4 @@ usermod -aG docker debian
 # Pull chunkr git repository
 git clone https://github.com/lumina-ai-inc/chunkr.git ./chunkr
 chown -R debian:debian ./chunkr
+
