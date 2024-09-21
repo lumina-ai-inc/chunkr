c1bea8770ca42ba037b262991b184ad9d298db9d
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-09-20 18:56:10 -0700

    convert from path

--- services/pdf-document-layout-analysis/src/data_model/PdfImages.py
+++ services/pdf-document-layout-analysis/src/data_model/PdfImages.py
@@ -1,8 +1,5 @@
 import os
 import shutil
-import requests
-import base64
-import io
 
 import cv2
 import numpy as np
@@ -10,15 +7,11 @@ from os import makedirs
 from os.path import join
 from pathlib import Path
 from PIL import Image
-from dotenv import load_dotenv
-
+from pdf2image import convert_from_path
 from pdf_features.PdfFeatures import PdfFeatures
-from pdf_features.convert_to_img import convert_file_to_images
-from pdf2image import convert_from_bytes
+
 from src.configuration import IMAGES_ROOT_PATH, XMLS_PATH
 
-# Load environment variables
-load_dotenv(override=True)
 
 class PdfImages:
     def __init__(self, pdf_features: PdfFeatures, pdf_images: list[Image]):
@@ -44,12 +37,12 @@ class PdfImages:
         shutil.rmtree(IMAGES_ROOT_PATH)
 
     @staticmethod
-    def from_pdf_path(pdf_path: str | Path, pdf_name: str = "", xml_file_name: str = "", density: int = 300, extension: str = "jpg"):
+    def from_pdf_path(pdf_path: str | Path, pdf_name: str = "", xml_file_name: str = "", density: int = 72, extension: str = "jpeg"):
         xml_path = Path(join(XMLS_PATH, xml_file_name)) if xml_file_name else None
 
         if xml_path and not xml_path.parent.exists():
             os.makedirs(xml_path.parent, exist_ok=True)
-        print(f"Creating PDF images from path: {pdf_path}")
+
         pdf_features: PdfFeatures = PdfFeatures.from_pdf_path(pdf_path, str(xml_path) if xml_path else None)
 
         if pdf_name:
@@ -57,17 +50,5 @@ class PdfImages:
         else:
             pdf_name = Path(pdf_path).parent.name if Path(pdf_path).name == "document.pdf" else Path(pdf_path).stem
             pdf_features.file_name = pdf_name
-
-        # Use the convert_file_to_images function
-        output_dir = Path(IMAGES_ROOT_PATH) / pdf_name
-        os.makedirs(output_dir, exist_ok=True)
-        convert_file_to_images(pdf_path, output_dir, density=density, extension=extension)
-
-        pdf_images = []
-        for image_file in sorted(output_dir.glob(f"*.{extension}")):
-            with Image.open(image_file) as img:
-                pdf_images.append(img.copy())
-            os.remove(image_file)  # Remove the temporary image file
-
+        pdf_images = convert_from_path(pdf_path, dpi=density, fmt=extension)
         return PdfImages(pdf_features, pdf_images)
-
