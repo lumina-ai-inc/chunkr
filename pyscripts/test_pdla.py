6fabe39ac167c8967f67b05f66fa200199eecb7d
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-09-20 18:56:10 -0700

    convert from path

--- pyscripts/test_pdla.py
+++ pyscripts/test_pdla.py
@@ -29,7 +29,7 @@ class TestPDLAServer(unittest.TestCase):
                 print(f"Processing PDF file: {pdf_path}")
                 with open(pdf_path, "rb") as pdf_file:
                     files = {"file": (pdf_path.name, pdf_file, "application/pdf")}
-                    data = {"fast": "false"}
+                    data = {"fast": "false", "density": 72, "extension": "jpeg"}
                     
                     response = requests.post(url, files=files, data=data)
                 
