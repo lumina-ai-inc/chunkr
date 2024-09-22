3d8c0ba304a3b696a0bbdca6f20060859b9aa76b
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-22 12:06:23 -0700

    added eta

--- services/task/tests/process.py
+++ services/task/tests/process.py
@@ -62,7 +62,8 @@ def send_files_to_process(pdf_path: str, json_path: str, service_url: str, outpu
         'segment_bbox_offset': 2,
         'segment_image_quality': 100,
         'segment_image_resize': None,
-        'ocr_strategy': 'auto'
+        'ocr_strategy': 'auto',
+        'ocr_on_formulas': False
     }
 
     start_time = time.time()
