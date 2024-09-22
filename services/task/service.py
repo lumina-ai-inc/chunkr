59df33f91ed8a2ac27c42b15e059d410a11dd92c
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-22 12:32:09 -0700

    ready to be used by the rest of the systems

--- services/task/service.py
+++ services/task/service.py
@@ -90,6 +90,7 @@ class Task:
         check_imagemagick_installed()
         self.ocr = PaddleOCR(use_angle_cls=True, lang="en",
                              ocr_order_method="tb-xy", show_log=False)
+        # todo: add lang support
         self.table_engine = PPStructure(
             recovery=True, return_ocr_result_in_table=True, layout=False, structure_version="PP-StructureV2", show_log=False)
         self.ocr_lock: threading.Lock = threading.Lock()
