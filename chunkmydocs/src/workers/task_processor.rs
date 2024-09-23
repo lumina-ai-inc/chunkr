16998edae9cb69248aad68fb7e14cf47d8a2d992
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-23 16:18:53 -0700

    done with ocr

--- chunkmydocs/src/workers/task_processor.rs
+++ chunkmydocs/src/workers/task_processor.rs
@@ -85,7 +85,7 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
             log_task(
                 task_id.clone(),
                 Status::Processing,
-                Some(format!("Segmenting | batch {} of {}", batch_number, split_temp_files.len())),
+                Some(format!("Segmenting | Batch {} of {}", batch_number, split_temp_files.len())),
                 None,
                 &pg_pool
             ).await?;
@@ -98,7 +98,7 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
             log_task(
                 task_id.clone(),
                 Status::Processing,
-                Some(format!("OCR | batch {} of {}", batch_number, split_temp_files.len())),
+                Some(format!("OCR: {} | Batch {} of {}", extraction_item.configuration.ocr_strategy, batch_number, split_temp_files.len())),
                 None,
                 &pg_pool
             ).await?;
