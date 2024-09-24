5d55713db092ab1afabef502306a45defce397f9
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-24 16:37:22 -0700

    repushing

--- docker/task/docker.sh
+++ docker/task/docker.sh
@@ -13,12 +13,12 @@ echo "------------------------"
 
 docker build --platform linux/amd64 -t $DOCKER_IMAGE_NAME:$SHA -f $CURRENT_DIR/Dockerfile .
 
-# if [ $? -eq 0 ]; then
-#     docker push $DOCKER_IMAGE_NAME:$SHA
+if [ $? -eq 0 ]; then
+    docker push $DOCKER_IMAGE_NAME:$SHA
 
-#     docker tag $DOCKER_IMAGE_NAME:$SHA $DOCKER_IMAGE_NAME:latest
-#     docker push $DOCKER_IMAGE_NAME:latest
-# else
-#     echo "Docker build failed. Skipping push."
-#     exit 1
-# fi
+    docker tag $DOCKER_IMAGE_NAME:$SHA $DOCKER_IMAGE_NAME:latest
+    docker push $DOCKER_IMAGE_NAME:latest
+else
+    echo "Docker build failed. Skipping push."
+    exit 1
+fi
