44533a9b0d60bab7fe46f597842bad0313e94d45
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-09-24 14:01:03 -0700

    separated out task workers

--- chunkmydocs/src/utils/workers/process.rs
+++ chunkmydocs/src/utils/workers/process.rs
@@ -1,18 +1,16 @@
-use chrono::{ DateTime, Utc };
-use chunkmydocs::models::rrq::queue::QueuePayload;
-use chunkmydocs::models::server::extract::ExtractionPayload;
-use chunkmydocs::models::server::segment::{ PdlaSegment, BaseSegment, Segment };
-use chunkmydocs::models::server::task::Status;
-use chunkmydocs::task::pdla::pdla_extraction;
-use chunkmydocs::task::process::process_segments;
-use chunkmydocs::task::pdf::split_pdf;
-use chunkmydocs::utils::configs::extraction_config;
-use chunkmydocs::utils::db::deadpool_postgres::{ create_pool, Client, Pool };
-use chunkmydocs::utils::json2mkd::json_2_mkd::hierarchical_chunking;
-use chunkmydocs::utils::rrq::consumer::consumer;
-use chunkmydocs::utils::storage::config_s3::create_client;
-use chunkmydocs::utils::storage::services::{ download_to_tempfile, upload_to_s3 };
-use std::{ io::Write, path::PathBuf };
+use crate::models::rrq::queue::QueuePayload;
+use crate::models::server::extract::ExtractionPayload;
+use crate::models::server::segment::{BaseSegment, PdlaSegment, Segment};
+use crate::models::server::task::Status;
+use crate::task::pdf::split_pdf;
+use crate::task::pdla::pdla_extraction;
+use crate::task::process::process_segments;
+use crate::utils::db::deadpool_postgres::{create_pool, Client, Pool};
+use crate::utils::json2mkd::json_2_mkd::hierarchical_chunking;
+use crate::utils::storage::config_s3::create_client;
+use crate::utils::storage::services::{download_to_tempfile, upload_to_s3};
+use chrono::{DateTime, Utc};
+use std::{io::Write, path::PathBuf};
 use tempdir::TempDir;
 use tempfile::NamedTempFile;
 
@@ -21,7 +19,7 @@ pub async fn log_task(
     status: Status,
     message: Option<String>,
     finished_at: Option<DateTime<Utc>>,
-    pool: &Pool
+    pool: &Pool,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let client: Client = pool.get().await?;
 
@@ -38,7 +36,7 @@ pub async fn log_task(
     Ok(())
 }
 
-async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>> {
     println!("Processing task");
     let s3_client = create_client().await?;
     let reqwest_client = reqwest::Client::new();
@@ -50,28 +48,30 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
     log_task(
         task_id.clone(),
         Status::Processing,
-        Some(format!("Task processing | Tries ({}/{})", payload.attempt, payload.max_attempts)),
+        Some(format!(
+            "Task processing | Tries ({}/{})",
+            payload.attempt, payload.max_attempts
+        )),
         None,
-        &pg_pool
-    ).await?;
+        &pg_pool,
+    )
+    .await?;
 
     let result: Result<(), Box<dyn std::error::Error>> = (async {
         let temp_file = download_to_tempfile(
             &s3_client,
             &reqwest_client,
             &extraction_item.input_location,
-            None
-        ).await?;
+            None,
+        )
+        .await?;
 
         let mut split_temp_files: Vec<PathBuf> = vec![];
         let split_temp_dir = TempDir::new("split_pdf")?;
 
         if let Some(batch_size) = extraction_item.batch_size {
-            split_temp_files = split_pdf(
-                temp_file.path(),
-                batch_size as usize,
-                split_temp_dir.path()
-            ).await?;
+            split_temp_files =
+                split_pdf(temp_file.path(), batch_size as usize, split_temp_dir.path()).await?;
         } else {
             split_temp_files.push(temp_file.path().to_path_buf());
         }
@@ -83,18 +83,31 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
         for temp_file in &split_temp_files {
             batch_number += 1;
             let mut segmentation_message = "Segmenting".to_string();
-            let mut ocr_message = format!("Performing OCR: {}", extraction_item.configuration.ocr_strategy);
+            let mut ocr_message = format!(
+                "Performing OCR: {}",
+                extraction_item.configuration.ocr_strategy
+            );
             if batch_number > 1 {
-                segmentation_message = format!("Segmenting | Batch {} of {}", batch_number, split_temp_files.len());
-                ocr_message = format!("Performing OCR: {} | Batch {} of {}", extraction_item.configuration.ocr_strategy, batch_number, split_temp_files.len());
+                segmentation_message = format!(
+                    "Segmenting | Batch {} of {}",
+                    batch_number,
+                    split_temp_files.len()
+                );
+                ocr_message = format!(
+                    "Performing OCR: {} | Batch {} of {}",
+                    extraction_item.configuration.ocr_strategy,
+                    batch_number,
+                    split_temp_files.len()
+                );
             }
             log_task(
                 task_id.clone(),
                 Status::Processing,
                 Some(segmentation_message),
                 None,
-                &pg_pool
-            ).await?;
+                &pg_pool,
+            )
+            .await?;
             let pdla_response = pdla_extraction(temp_file, extraction_item.model.clone()).await?;
             let pdla_segments: Vec<PdlaSegment> = serde_json::from_str(&pdla_response)?;
             let base_segments: Vec<BaseSegment> = pdla_segments
@@ -106,14 +119,16 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
                 Status::Processing,
                 Some(ocr_message),
                 None,
-                &pg_pool
-            ).await?;
+                &pg_pool,
+            )
+            .await?;
             let mut segments: Vec<Segment> = process_segments(
                 temp_file,
                 &base_segments,
                 &extraction_item.image_folder_location,
-                &extraction_item.configuration.ocr_strategy
-            ).await?;
+                &extraction_item.configuration.ocr_strategy,
+            )
+            .await?;
 
             for item in &mut segments {
                 item.page_number = item.page_number + page_offset;
@@ -122,15 +137,18 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
             page_offset += extraction_item.batch_size.unwrap_or(1) as u32;
         }
 
-        let chunks = hierarchical_chunking(
-            combined_output,
-            extraction_item.target_chunk_length
-        ).await?;
+        let chunks =
+            hierarchical_chunking(combined_output, extraction_item.target_chunk_length).await?;
 
         let mut output_temp_file = NamedTempFile::new()?;
         output_temp_file.write_all(serde_json::to_string(&chunks)?.as_bytes())?;
 
-        upload_to_s3(&s3_client, &extraction_item.output_location, &output_temp_file.path()).await?;
+        upload_to_s3(
+            &s3_client,
+            &extraction_item.output_location,
+            &output_temp_file.path(),
+        )
+        .await?;
 
         if temp_file.path().exists() {
             if let Err(e) = std::fs::remove_file(temp_file.path()) {
@@ -144,7 +162,8 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
         }
 
         Ok(())
-    }).await;
+    })
+    .await;
 
     match result {
         Ok(_) => {
@@ -154,8 +173,9 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
                 Status::Succeeded,
                 Some("Task succeeded".to_string()),
                 Some(Utc::now()),
-                &pg_pool
-            ).await?;
+                &pg_pool,
+            )
+            .await?;
             Ok(())
         }
         Err(e) => {
@@ -167,18 +187,11 @@ async fn process(payload: QueuePayload) -> Result<(), Box<dyn std::error::Error>
                     Status::Failed,
                     Some("Task failed".to_string()),
                     Some(Utc::now()),
-                    &pg_pool
-                ).await?;
+                    &pg_pool,
+                )
+                .await?;
             }
             Err(e)
         }
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = extraction_config::Config::from_env()?;
-    println!("Starting task processor");
-    consumer(process, config.extraction_queue, 1, 600).await?;
-    Ok(())
 }
