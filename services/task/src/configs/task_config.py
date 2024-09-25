1ae78389b1757c699f5e3bcef3aed20d0b5a6dd7
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-24 23:12:05 -0700

    task ocr parsed to float

--- services/task/src/configs/task_config.py
+++ services/task/src/configs/task_config.py
@@ -3,4 +3,4 @@ import os
 
 dotenv.load_dotenv(override=True)
 
-TASK__OCR_CONFIDENCE_THRESHOLD = os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") if os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") else 0.85
+TASK__OCR_CONFIDENCE_THRESHOLD: float = float(os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") if os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") else 0.85)
