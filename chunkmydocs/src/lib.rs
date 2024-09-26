49ee2cedd5cbaf9417b5e3c037cec0f820c67d32
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-09-26 11:21:15 -0700

    rm cron from self deploy

--- chunkmydocs/src/lib.rs
+++ chunkmydocs/src/lib.rs
@@ -5,49 +5,48 @@ use actix_web::get;
 use actix_web::middleware::Logger;
 use actix_web::Error;
 use actix_web::HttpRequest;
-use actix_web::{ web, App, HttpServer };
-use diesel_migrations::{ embed_migrations, EmbeddedMigrations, MigrationHarness };
+use actix_web::{web, App, HttpServer};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use env_logger::Env;
 use std::time::Duration;
-use tokio::time; 
-pub mod task;
+use tokio::time;
 pub mod middleware;
 pub mod models;
 pub mod routes;
+pub mod task;
 pub mod utils;
 
 use middleware::auth::AuthMiddlewareFactory;
-use routes::user::get_or_create_user;
 use routes::health::health_check;
-use routes::task::{ create_extraction_task, get_task_status };
+use routes::stripe::{
+    create_setup_intent, create_stripe_session, get_invoice_detail, get_monthly_usage,
+    get_user_invoices, stripe_webhook,
+};
+use routes::task::{create_extraction_task, get_task_status};
 use routes::tasks::get_tasks_status;
 use routes::usage::get_usage;
-use routes::stripe::{
-    create_setup_intent,
-    stripe_webhook,
-    create_stripe_session,
-    get_user_invoices,
-    get_invoice_detail,
-    get_monthly_usage,
-};
+use routes::user::get_or_create_user;
 use utils::db::deadpool_postgres;
-use utils::storage::config_s3::create_client;
 use utils::server::admin_user::get_or_create_admin_user;
-use utoipa::{ openapi::security::{ ApiKey, ApiKeyValue, SecurityScheme }, Modify, OpenApi };
-use utoipa_redoc::{ Redoc, Servable };
-use utoipa_swagger_ui::SwaggerUi;
+use utils::storage::config_s3::create_client;
 use utils::stripe::invoicer::invoice;
+use utoipa::{
+    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
+    Modify, OpenApi,
+};
+use utoipa_redoc::{Redoc, Servable};
+use utoipa_swagger_ui::SwaggerUi;
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
 fn run_migrations(url: &str) {
     use diesel::prelude::*;
 
     let mut conn = diesel::pg::PgConnection::establish(url).expect("Failed to connect to database");
-    conn.run_pending_migrations(MIGRATIONS).expect("Failed to run migrations");
+    conn.run_pending_migrations(MIGRATIONS)
+        .expect("Failed to run migrations");
 
     println!("Migrations ran successfully");
 }
-
 
 #[derive(OpenApi)]
 #[openapi(
@@ -93,12 +92,13 @@ struct SecurityAddon;
 
 impl Modify for SecurityAddon {
     fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
-        let components = openapi.components
+        let components = openapi
+            .components
             .as_mut()
             .expect("Safe to expect since the component was already registered");
         components.add_security_scheme(
             "api_key",
-            SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("Authorization")))
+            SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("Authorization"))),
         );
     }
 }
@@ -113,25 +113,24 @@ pub fn main() -> std::io::Result<()> {
         let pg_pool = deadpool_postgres::create_pool(); // Wrap in Arc
         let s3_client = create_client().await.expect("Failed to create S3 client");
         run_migrations(&std::env::var("PG__URL").expect("PG__URL must be set in .env file"));
-        get_or_create_admin_user(&pg_pool).await.expect("Failed to create admin user");
+        get_or_create_admin_user(&pg_pool)
+            .await
+            .expect("Failed to create admin user");
 
         fn handle_multipart_error(err: MultipartError, _: &HttpRequest) -> Error {
             println!("Multipart error: {}", err);
             Error::from(err)
         }
 
-        let max_size: usize = std::env
-            ::var("MAX_TOTAL_LIMIT")
+        let max_size: usize = std::env::var("MAX_TOTAL_LIMIT")
             .unwrap_or_else(|_| "10485760".to_string()) // Default to 10 MB if not set
             .parse()
             .expect("MAX_TOTAL_LIMIT must be a valid usize");
-        let max_memory_size: usize = std::env
-            ::var("MAX_MEMORY_LIMIT")
+        let max_memory_size: usize = std::env::var("MAX_MEMORY_LIMIT")
             .unwrap_or_else(|_| "10485760".to_string()) // Default to 10 MB if not set
             .parse()
             .expect("MAX_MEMORY_LIMIT must be a valid usize");
-        let timeout: usize = std::env
-            ::var("TIMEOUT")
+        let timeout: usize = std::env::var("TIMEOUT")
             .unwrap_or_else(|_| "600".to_string())
             .parse()
             .expect("TIMEOUT must be a valid usize");
@@ -139,20 +138,21 @@ pub fn main() -> std::io::Result<()> {
 
         env_logger::init_from_env(Env::default().default_filter_or("info"));
         let pg_pool_clone = deadpool_postgres::create_pool(); // Clone the Arc
-        actix_web::rt::spawn(async move {
-            let today = chrono::Utc::now().date_naive();
-            let mut interval = time::interval(Duration::from_secs(86400)); // 86400 seconds = 24 hours
-            loop {
-                interval.tick().await;
-                println!("Processing daily invoices");
-                if let Err(e) = invoice(&pg_pool_clone, Some(today)).await {
-                    eprintln!("Error processing daily invoices: {}", e);
+        if std::env::var("STRIPE__API_KEY").is_ok() {
+            actix_web::rt::spawn(async move {
+                let today = chrono::Utc::now().date_naive();
+                let mut interval = time::interval(Duration::from_secs(86400)); // 86400 seconds = 24 hours
+                loop {
+                    interval.tick().await;
+                    println!("Processing daily invoices");
+                    if let Err(e) = invoice(&pg_pool_clone, Some(today)).await {
+                        eprintln!("Error processing daily invoices: {}", e);
+                    }
                 }
-            }
-        });
+            });
+        }
         HttpServer::new(move || {
             let mut app = App::new()
-            
                 .wrap(Cors::permissive())
                 .wrap(Logger::default())
                 .wrap(Logger::new("%a %{User-Agent}i"))
@@ -162,20 +162,17 @@ pub fn main() -> std::io::Result<()> {
                     MultipartFormConfig::default()
                         .total_limit(max_size)
                         .memory_limit(max_memory_size)
-                        .error_handler(handle_multipart_error)
+                        .error_handler(handle_multipart_error),
                 )
                 .service(Redoc::with_url("/redoc", ApiDoc::openapi()))
                 .route("/", web::get().to(health_check))
                 .route("/health", web::get().to(health_check))
                 .service(
-                    SwaggerUi::new("/swagger-ui/{_:.*}").url(
-                        "/docs/openapi.json",
-                        ApiDoc::openapi()
-                    )
+                    SwaggerUi::new("/swagger-ui/{_:.*}")
+                        .url("/docs/openapi.json", ApiDoc::openapi()),
                 );
 
-            let api_scope = web
-                ::scope("/api/v1")
+            let api_scope = web::scope("/api/v1")
                 .wrap(AuthMiddlewareFactory)
                 .route("/user", web::get().to(get_or_create_user))
                 .route("/task", web::post().to(create_extraction_task))
@@ -187,8 +184,7 @@ pub fn main() -> std::io::Result<()> {
             if std::env::var("STRIPE__API_KEY").is_ok() {
                 app = app.route("/stripe/webhook", web::post().to(stripe_webhook));
 
-                let stripe_scope = web
-                    ::scope("/stripe")
+                let stripe_scope = web::scope("/stripe")
                     .wrap(AuthMiddlewareFactory)
                     .route("/create-setup-intent", web::get().to(create_setup_intent))
                     .route("/create-session", web::get().to(create_stripe_session))
@@ -200,8 +196,9 @@ pub fn main() -> std::io::Result<()> {
 
             app.service(api_scope)
         })
-            .bind("0.0.0.0:8000")?
-            .keep_alive(timeout)
-            .run().await
+        .bind("0.0.0.0:8000")?
+        .keep_alive(timeout)
+        .run()
+        .await
     })
 }
