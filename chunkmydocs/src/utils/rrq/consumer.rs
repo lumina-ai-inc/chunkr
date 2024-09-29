34a9bec6c91a86a7b19a8810077444ae6e8ac55d
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-28 21:36:57 -0700

    updating queue consumer

--- chunkmydocs/src/utils/rrq/consumer.rs
+++ chunkmydocs/src/utils/rrq/consumer.rs
@@ -1,22 +1,29 @@
 use crate::models::rrq::{
-    consume::{ConsumePayload, ConsumeResponse},
+    consume::ConsumePayload,
     queue::QueuePayload,
-    status::{StatusPayload, StatusResult},
+    status::{ StatusPayload, StatusResult },
 };
-use crate::utils::rrq::service::{complete, consume, health};
+use crate::utils::rrq::service::{ complete, consume, health };
 use tokio::time::Duration;
 
 pub async fn consumer<F, Fut>(
     process_fn: F,
     queue_name: String,
     item_count: i64,
-    expiration_seconds: u64,
-) -> Result<(), Box<dyn std::error::Error>>
-where
-    F: Fn(QueuePayload) -> Fut + Sync + 'static,
-    Fut: std::future::Future<Output = Result<(), Box<dyn std::error::Error>>>,
+    expiration_seconds: u64
+)
+    -> Result<(), Box<dyn std::error::Error>>
+    where
+        F: Fn(QueuePayload) -> Fut + Sync + 'static,
+        Fut: std::future::Future<Output = Result<(), Box<dyn std::error::Error>>>
 {
-    let _ = health().await?;
+    let _ = match health().await {
+        Ok(_) => (),
+        Err(e) => {
+            println!("Error checking health: {}", e);
+            return Err(e);
+        }
+    };
 
     let consumer_payload = ConsumePayload {
         consumer_id: uuid::Uuid::new_v4().to_string(),
@@ -26,48 +33,55 @@ where
     };
 
     loop {
-        let consume_payloads: Vec<ConsumeResponse> = consume(consumer_payload.clone()).await?;
-        if consume_payloads.is_empty() {
-            println!("No content received, waiting for 1 second before trying again...");
-            tokio::time::sleep(Duration::from_secs(1)).await;
-            continue;
-        }
-
-        for consume_payload in consume_payloads {
-            let mut status_payload = StatusPayload {
-                item_id: consume_payload.queue_item.item_id.clone(),
-                item_index: consume_payload.item_index,
-                consumer_id: consumer_payload.consumer_id.clone(),
-                queue_name: queue_name.clone(),
-                result: StatusResult::Failure,
-                message: None,
-            };
-
-            let mut payloads = Vec::new();
-            match process_fn(consume_payload.queue_item).await {
-                Ok(_) => {
-                    status_payload.result = StatusResult::Success;
+        match consume(consumer_payload.clone()).await {
+            Ok(consume_payloads) => {
+                if consume_payloads.is_empty() {
+                    println!("No content received, waiting for 1 second before trying again...");
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                    continue;
                 }
-                Err(e) => {
-                    println!("Error processing queue item: {}", e);
-                    status_payload.message = Some(e.to_string());
-                    status_payload.result = StatusResult::Failure;
-                }
-            }
-            payloads.push(status_payload);
+                for consume_payload in consume_payloads {
+                    let mut status_payload = StatusPayload {
+                        item_id: consume_payload.queue_item.item_id.clone(),
+                        item_index: consume_payload.item_index,
+                        consumer_id: consumer_payload.consumer_id.clone(),
+                        queue_name: queue_name.clone(),
+                        result: StatusResult::Failure,
+                        message: None,
+                    };
 
-            match complete(payloads.clone()).await {
-                Ok(_) => (),
-                Err(e) => {
-                    println!("Failed to complete queue item: {}. Retrying...", e);
-                    payloads.iter_mut().for_each(|p| {
-                        p.result = StatusResult::Success;
-                    });
-                    match complete(payloads).await {
-                        Ok(_) => println!("Retry successful"),
-                        Err(retry_e) => println!("Retry failed: {}", retry_e),
+                    let mut payloads = Vec::new();
+                    match process_fn(consume_payload.queue_item).await {
+                        Ok(_) => {
+                            status_payload.result = StatusResult::Success;
+                        }
+                        Err(e) => {
+                            println!("Error processing queue item: {}", e);
+                            status_payload.message = Some(e.to_string());
+                            status_payload.result = StatusResult::Failure;
+                        }
+                    }
+                    payloads.push(status_payload);
+
+                    match complete(payloads.clone()).await {
+                        Ok(_) => (),
+                        Err(e) => {
+                            println!("Failed to complete queue item: {}. Retrying...", e);
+                            payloads.iter_mut().for_each(|p| {
+                                p.result = StatusResult::Success;
+                            });
+                            match complete(payloads).await {
+                                Ok(_) => println!("Retry successful"),
+                                Err(retry_e) => println!("Retry failed: {}", retry_e),
+                            }
+                        }
                     }
                 }
+            }
+            Err(e) => {
+                println!("Error consuming queue item: {}", e);
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                continue;
             }
         }
     }
