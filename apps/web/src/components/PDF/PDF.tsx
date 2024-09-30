b572fa503db061b523d8b4d0f6fa3887e941621e
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-09-29 18:48:37 -0700

    added pollyfill for promise with resolvers

--- apps/web/src/components/PDF/PDF.tsx
+++ apps/web/src/components/PDF/PDF.tsx
@@ -12,6 +12,24 @@ import {
 } from "../../models/chunk.model";
 import "./PDF.css";
 
+declare global {
+  interface PromiseConstructor {
+    withResolvers<T>(): { promise: Promise<T>; resolve: (value: T | PromiseLike<T>) => void; reject: (reason?: unknown) => void };
+  }
+}
+
+if (typeof Promise.withResolvers !== 'function') {
+  Promise.withResolvers = function <T>() {
+    let resolve!: (value: T | PromiseLike<T>) => void;
+    let reject!: (reason?: unknown) => void;
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const options = {
