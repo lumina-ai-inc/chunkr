b0c974f7c94df4043521af01bc1b467c1fe0b534
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-10 11:56:44 -0700

    added queue for ocr engines

--- services/task/src/configs/task_config.py
+++ services/task/src/configs/task_config.py
@@ -5,3 +5,5 @@ dotenv.load_dotenv(override=True)
 
 TASK__OCR_CONFIDENCE_THRESHOLD: float = float(os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") if os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") else 0.85)
 TASK__OCR_MAX_SIZE: int = int(os.getenv("TASK__OCR_MAX_SIZE") if os.getenv("TASK__OCR_MAX_SIZE") else 4000)
+TASK__OCR_POOL_SIZE: int = int(os.getenv("TASK__OCR_POOL_SIZE") if os.getenv("TASK__OCR_POOL_SIZE") else 4)
+TASK__TABLE_ENGINE_POOL_SIZE: int = int(os.getenv("TASK__TABLE_ENGINE_POOL_SIZE") if os.getenv("TASK__TABLE_ENGINE_POOL_SIZE") else 4)
