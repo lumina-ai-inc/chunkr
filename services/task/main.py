2973bdbde2bb01850bb5f5c2588a3970ecd33f90
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-10 11:34:34 -0700

    ocr still fucky

--- services/task/main.py
+++ services/task/main.py
@@ -164,4 +164,4 @@ async def process_segments_with_ocr(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
