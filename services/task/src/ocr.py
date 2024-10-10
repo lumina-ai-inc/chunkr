c9e798f21936101d901194800f336ceb6061d203
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-10 13:27:44 -0700

    using ocr service

--- services/task/src/ocr.py
+++ services/task/src/ocr.py
@@ -1,118 +1,19 @@
 from bs4 import BeautifulSoup
 import cv2
-from paddleocr import PaddleOCR, PPStructure
 from pathlib import Path
-from queue import Queue
-import threading
+import requests
 from typing import List
 
-from src.configs.task_config import TASK__OCR_MAX_SIZE, TASK__OCR_POOL_SIZE, TASK__TABLE_ENGINE_POOL_SIZE
+from src.configs.task_config import TASK__OCR_SERVICE_URL
 from src.models.ocr_model import OCRResult, OCRResponse, BoundingBox
 
 
-class OCRPool:
-    # todo: add lang support
-    def __init__(self, ocr_pool_size: int = 4, table_engine_pool_size: int = 4):
-        self.ocr_engine_queue = Queue(maxsize=ocr_pool_size)
-        self.table_engine_queue = Queue(maxsize=table_engine_pool_size)
-        self.ocr_engine_lock = threading.Lock()
-        self.table_engine_lock = threading.Lock()
-
-        print("Creating OCR engines")
-        for _ in range(ocr_pool_size):
-            ocr = self.__create_ocr_engine()
-            self.ocr_engine_queue.put(ocr)
-
-        print("Creating table engines")
-        for _ in range(table_engine_pool_size):
-            table_engine = self.__create_ocr_engine()
-            self.table_engine_queue.put(table_engine)
-
-    def __create_ocr_engine(self) -> PaddleOCR:
-        with self.ocr_engine_lock:
-            return PaddleOCR(use_angle_cls=True, lang="en", ocr_order_method="tb-xy", show_log=False)
-
-    def __create_table_engine(self) -> PPStructure:
-        with self.table_engine_lock:
-            return PPStructure(recovery=True, return_ocr_result_in_table=True, layout=False, structure_version="PP-StructureV2", show_log=False)
-
-    def get_ocr_engine(self) -> PaddleOCR:
-        with self.ocr_engine_lock:
-            return self.ocr_engine_queue.get()
-
-    def __replace_ocr_engine(self, ocr: PaddleOCR):
-        # Note: del engine to stop memory leak https://github.com/PaddlePaddle/PaddleOCR/issues/11639
-        with self.ocr_engine_lock:
-            del ocr
-            new_ocr = self.__create_ocr_engine()
-            self.ocr_engine_queue.put(new_ocr)
-
-    def __replace_table_engine(self, table_engine: PPStructure):
-        # Note: del engine to stop memory leak https://github.com/PaddlePaddle/PaddleOCR/issues/11639
-        with self.table_engine_lock:
-            del table_engine
-            new_table_engine = self.__create_table_engine()
-            self.table_engine_queue.put(new_table_engine)
-
-    def return_ocr_engine(self, ocr: PaddleOCR):
-        with self.ocr_engine_lock:
-            self.ocr_engine_queue.put(ocr)
-
-    def get_table_engine(self) -> PPStructure:
-        with self.table_engine_lock:
-            return self.table_engine_queue.get()
-
-    def return_table_engine(self, table_engine: PPStructure):
-        with self.table_engine_lock:
-            self.table_engine_queue.put(table_engine)
-
-
-ocr_pool = OCRPool(ocr_pool_size=TASK__OCR_POOL_SIZE, table_engine_pool_size=TASK__TABLE_ENGINE_POOL_SIZE)
-
-
-def calculate_slice_params(image_size, max_size):
-    width, height = image_size
-    if width <= max_size and height <= max_size:
-        return None
-
-    h_stride = max(300, width // 8)
-    v_stride = max(300, height // 8)
-
-    merge_x_thres = max(20, h_stride // 10)
-    merge_y_thres = max(20, v_stride // 10)
-
-    return {
-        'horizontal_stride': h_stride,
-        'vertical_stride': v_stride,
-        'merge_x_thres': merge_x_thres,
-        'merge_y_thres': merge_y_thres
-    }
-
-
 def ppocr_raw(image_path: Path) -> list:
-    ocr = ocr_pool.get_ocr_engine()
-    return ocr.ocr(str(image_path))
+    return requests.post(f"{TASK__OCR_SERVICE_URL}/ocr", files={"image": image_path.read_bytes()}).json()
 
 
 def ppocr(image_path: Path) -> List[OCRResult]:
-    ocr = ocr_pool.get_ocr_engine()
-
-    max_size = TASK__OCR_MAX_SIZE
-    img = cv2.imread(str(image_path))
-    if img is None:
-        return []
-
-    height, width = img.shape[:2]
-    slice_params = calculate_slice_params((width, height), max_size)
-
-    if slice_params:
-        raw_results = ocr.ocr(str(image_path), det=True,
-                              rec=True, cls=False, slice=slice_params)
-    else:
-        raw_results = ocr.ocr(str(image_path))
-
-    if not raw_results or not raw_results[0]:
-        return []
+    raw_results = ppocr_raw(image_path)
 
     ocr_results = []
     for result in raw_results[0]:
@@ -130,28 +31,17 @@ def ppocr(image_path: Path) -> List[OCRResult]:
 
 
 def ppstructure_table_raw(image_path: Path) -> list:
-    table_engine = ocr_pool.get_table_engine()
-
-    img = cv2.imread(str(image_path))
-    result = table_engine(img)
-    for line in result:
-        line.pop('img')
-    return result
+    return requests.post(f"{TASK__OCR_SERVICE_URL}/ocr/table", files={"image": image_path.read_bytes()}).json()
 
 
 def ppstructure_table(image_path: Path) -> OCRResponse:
-    table_engine = ocr_pool.get_table_engine()
+    raw_results = ppstructure_table_raw(image_path)
 
-    img = cv2.imread(str(image_path))
-    result = table_engine(img)
-
-    table_result = result[0] if result else None
-
-    if not table_result:
+    if not raw_results:
         return OCRResponse(results=[], html="")
 
-    cell_bbox_raw = table_result['res'].get('cell_bbox', [])
-    html = table_result['res'].get('html', "")
+    cell_bbox_raw = raw_results['res'].get('cell_bbox', [])
+    html = raw_results['res'].get('html', "")
 
     soup = BeautifulSoup(html, 'html.parser')
     cells = soup.find_all(['td', 'th'])
