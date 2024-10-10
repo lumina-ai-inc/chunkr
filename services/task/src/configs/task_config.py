13e43d4364943f7b2e3c48ac69b96b392abe0680
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-10 13:27:44 -0700

    using ocr service

--- services/task/src/configs/task_config.py
+++ services/task/src/configs/task_config.py
@@ -4,6 +4,4 @@ import os
 dotenv.load_dotenv(override=True)
 
 TASK__OCR_CONFIDENCE_THRESHOLD: float = float(os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") if os.getenv("TASK__OCR_CONFIDENCE_THRESHOLD") else 0.85)
-TASK__OCR_MAX_SIZE: int = int(os.getenv("TASK__OCR_MAX_SIZE") if os.getenv("TASK__OCR_MAX_SIZE") else 4000)
-TASK__OCR_POOL_SIZE: int = int(os.getenv("TASK__OCR_POOL_SIZE") if os.getenv("TASK__OCR_POOL_SIZE") else 4)
-TASK__TABLE_ENGINE_POOL_SIZE: int = int(os.getenv("TASK__TABLE_ENGINE_POOL_SIZE") if os.getenv("TASK__TABLE_ENGINE_POOL_SIZE") else 4)
+TASK__OCR_SERVICE_URL: str = os.getenv("TASK__OCR_SERVICE_URL") if os.getenv("TASK__OCR_SERVICE_URL") else "http://localhost:8000"
