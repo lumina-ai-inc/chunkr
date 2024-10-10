6aec724bb0145fb1b7458b0a77965813d7ecd5e8
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-10 13:43:15 -0700

    ocr test cmds added

---
+++ services/ocr/cmds.sh
@@ -0,0 +1,3 @@
+curl -X POST "http://localhost:8000/ocr" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "image=@./sample/sample.jpg"
+
+curl -X POST "http://localhost:8000/ocr" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "image=@./sample/sample.jpg"
