1db9e76956b5ee0c4c82a8b3891187fcb5c36ade
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-10-11 00:29:03 -0700

    new login strat

--- services/task/src/configs/aws_config.py
+++ services/task/src/configs/aws_config.py
@@ -7,3 +7,8 @@ AWS__ENDPOINT = os.getenv("AWS__ENDPOINT")
 AWS__ACCESS_KEY = os.getenv("AWS__ACCESS_KEY")
 AWS__SECRET_KEY = os.getenv("AWS__SECRET_KEY")
 AWS__REGION = os.getenv("AWS__REGION")
+
+
+TASK__AWS__ACCESS_KEY = os.getenv("TASK__AWS__ACCESS_KEY")
+TASK__AWS__SECRET_KEY = os.getenv("TASK__AWS__SECRET_KEY")
+TASK__AWS__REGION = os.getenv("TASK__AWS__REGION")
