25a5330c7977b302337890bd5d4a5c6a401d3cad
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-11 15:46:03 -0700

    removed print statements

--- services/task/src/configs/textract_config.py
+++ services/task/src/configs/textract_config.py
@@ -1,22 +1,15 @@
-import boto3
 import dotenv
 import os
 import subprocess
 
 dotenv.load_dotenv(override=True)
 
-TEXTRACT__AWS_ACCESS_KEY = os.getenv("TEXTRACT__AWS_ACCESS_KEY")
-TEXTRACT__AWS_SECRET_KEY = os.getenv("TEXTRACT__AWS_SECRET_KEY")
-TEXTRACT__AWS_REGION = os.getenv("TEXTRACT__AWS_REGION")
-
 def login_aws():
     TEXTRACT__AWS_ACCESS_KEY = os.getenv("TEXTRACT__AWS_ACCESS_KEY")
     TEXTRACT__AWS_SECRET_KEY = os.getenv("TEXTRACT__AWS_SECRET_KEY")
     TEXTRACT__AWS_REGION = os.getenv("TEXTRACT__AWS_REGION")
-    print("values:", TEXTRACT__AWS_ACCESS_KEY, TEXTRACT__AWS_SECRET_KEY, TEXTRACT__AWS_REGION)
-    # boto3.setup_default_session(aws_access_key_id=TEXTRACT__AWS_ACCESS_KEY, aws_secret_access_key=TEXTRACT__AWS_SECRET_KEY, region_name=TEXTRACT__AWS_REGION)
 
-    if TEXTRACT__AWS_ACCESS_KEY:
+    if TEXTRACT__AWS_ACCESS_KEY and TEXTRACT__AWS_SECRET_KEY and TEXTRACT__AWS_REGION:
         try:
             subprocess.run(["aws", "configure", "set", "aws_access_key_id", TEXTRACT__AWS_ACCESS_KEY], check=True)
             subprocess.run(["aws", "configure", "set", "aws_secret_access_key", TEXTRACT__AWS_SECRET_KEY], check=True)
