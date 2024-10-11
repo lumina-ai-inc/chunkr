19aa569427436bde91be986c63574aa6a6fec378
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-11 15:36:49 -0700

    updating login

--- services/task/src/configs/textract_config.py
+++ services/task/src/configs/textract_config.py
@@ -14,16 +14,16 @@ def login_aws():
     TEXTRACT__AWS_SECRET_KEY = os.getenv("TEXTRACT__AWS_SECRET_KEY")
     TEXTRACT__AWS_REGION = os.getenv("TEXTRACT__AWS_REGION")
     print("values:", TEXTRACT__AWS_ACCESS_KEY, TEXTRACT__AWS_SECRET_KEY, TEXTRACT__AWS_REGION)
-    boto3.setup_default_session(aws_access_key_id=TEXTRACT__AWS_ACCESS_KEY, aws_secret_access_key=TEXTRACT__AWS_SECRET_KEY, region_name=TEXTRACT__AWS_REGION)
+    # boto3.setup_default_session(aws_access_key_id=TEXTRACT__AWS_ACCESS_KEY, aws_secret_access_key=TEXTRACT__AWS_SECRET_KEY, region_name=TEXTRACT__AWS_REGION)
 
-    # if TEXTRACT__AWS_ACCESS_KEY:
-    #     try:
-    #         subprocess.run(["aws", "configure", "set", "aws_access_key_id", TEXTRACT__AWS_ACCESS_KEY], check=True)
-    #         subprocess.run(["aws", "configure", "set", "aws_secret_access_key", TEXTRACT__AWS_SECRET_KEY], check=True)
-    #         subprocess.run(["aws", "configure", "set", "default.region", TEXTRACT__AWS_REGION], check=True)
-    #         print("AWS login successful for TEXTRACT-specific credentials.")
-    #     except subprocess.CalledProcessError:
-    #         print("Error: AWS login failed for TEXTRACT-specific credentials.")
-    # else:
-    #     print("No TEXTRACT-specific AWS credentials found.")
+    if TEXTRACT__AWS_ACCESS_KEY:
+        try:
+            subprocess.run(["aws", "configure", "set", "aws_access_key_id", TEXTRACT__AWS_ACCESS_KEY], check=True)
+            subprocess.run(["aws", "configure", "set", "aws_secret_access_key", TEXTRACT__AWS_SECRET_KEY], check=True)
+            subprocess.run(["aws", "configure", "set", "default.region", TEXTRACT__AWS_REGION], check=True)
+            print("AWS login successful for TEXTRACT-specific credentials.")
+        except subprocess.CalledProcessError:
+            print("Error: AWS login failed for TEXTRACT-specific credentials.")
+    else:
+        print("No TEXTRACT-specific AWS credentials found.")
     
