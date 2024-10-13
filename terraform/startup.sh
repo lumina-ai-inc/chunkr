e8c686b0887cfc8b06be99478ee981c1492cc971
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-13 12:55:13 -0700

    updated uv deps

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -17,6 +17,15 @@ apt-get install -y docker-compose
 git clone https://github.com/lumina-ai-inc/chunkr.git ./chunkr
 chown -R debian:debian ./chunkr
 
+# Install gh
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
 # Install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
