5577ed139950903ce5e4d7a54d2e9465fe991728
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-10-17 15:48:14 -0700

    preprocess

--- chunkmydocs/src/utils/services/ocr.rs
+++ chunkmydocs/src/utils/services/ocr.rs
@@ -5,19 +5,19 @@ use crate::utils::storage::services::download_to_tempfile;
 use aws_sdk_s3::Client as S3Client;
 use reqwest::Client as ReqwestClient;
 
-// pub async fn download_and_ocr(
-//     s3_client: &S3Client,
-//     reqwest_client: &ReqwestClient,
-//     image_location: &str,
-// ) -> Result<Vec<OCRResult>, Box<dyn std::error::Error>> {
-//     println!("Downloading and OCRing image: {:?}", image_location);
-//     let temp_file = download_to_tempfile(s3_client, reqwest_client, image_location, None).await?;
+pub async fn download_and_ocr(
+    s3_client: &S3Client,
+    reqwest_client: &ReqwestClient,
+    image_location: &str,
+) -> Result<Vec<OCRResult>, Box<dyn std::error::Error>> {
+    println!("Downloading and OCRing image: {:?}", image_location);
+    let temp_file = download_to_tempfile(s3_client, reqwest_client, image_location, None).await?;
 
-//     let ocr_results = call_rapid_ocr_api(temp_file.path()).await?;
-//     let table_structures = recognize_table(binarized_temp_file.path()).await?;
+    let ocr_results = call_rapid_ocr_api(temp_file.path()).await?;
+    // let table_structures = recognize_table(binarized_temp_file.path()).await?;
 
-//     temp_file.close()?;
-//     binarized_temp_file.close()?;
+    // temp_file.close()?;
+    // binarized_temp_file.close()?;
 
-//     Ok(ocr_results)
-// }
+    Ok(ocr_results)
+}
