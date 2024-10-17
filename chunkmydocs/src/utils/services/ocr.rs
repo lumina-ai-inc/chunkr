d79c33abfbf072955f5cbf1168ed5be9e9cd7670
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-10-17 14:02:57 -0700

    done

--- chunkmydocs/src/utils/services/ocr.rs
+++ chunkmydocs/src/utils/services/ocr.rs
@@ -1,5 +1,6 @@
 use crate::models::server::segment::OCRResult;
 use crate::utils::services::rapid_ocr::call_rapid_ocr_api;
+use crate::utils::services::table_struct::recognize_table;
 use crate::utils::storage::services::download_to_tempfile;
 use aws_sdk_s3::Client as S3Client;
 use reqwest::Client as ReqwestClient;
@@ -12,5 +13,6 @@ pub async fn download_and_ocr(
     println!("Downloading and OCRing image: {:?}", image_location);
     let temp_file = download_to_tempfile(s3_client, reqwest_client, image_location, None).await?;
     let ocr_results = call_rapid_ocr_api(&temp_file.path()).await?;
+    let table_structures = recognize_table(&temp_file.path()).await?;
     Ok(ocr_results)
 }
