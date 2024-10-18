c6c8c9dd6a83d307e8cfc791bcd6731e394c9917
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-17 22:02:03 -0700

    removed model downloading from docker

--- services/table-ocr/__init__.py
+++ services/table-ocr/__init__.py
@@ -1,1 +1,9 @@
+from table_structure import structure_model
 
+def main():
+    print("Starting model loading...")
+    print(structure_model)
+    print("Model loaded")
+
+if __name__ == "__main__":
+    main()
