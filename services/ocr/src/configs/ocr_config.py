b871214063a98824bd7ddf6bb4e0c39668859ab6
Ishaan <60672644+ishaan99k@users.noreply.github.com>
2024-10-21 13:08:32 -0700

    cleanup

--- services/ocr/src/configs/ocr_config.py
+++
@@ -1,6 +0,0 @@
-import dotenv
-import os
-
-dotenv.load_dotenv(override=True)
-
-OCR__MAX_SIZE: int = int(os.getenv("OCR__MAX_SIZE") if os.getenv("OCR__MAX_SIZE") else 4000)
--- services/task/tests/spell_check.py
+++
@@ -1,5 +0,0 @@
-import autocorrect
-
-spell = autocorrect.Speller(only_replacements=True)
-
-print(spell("are weighed against the loss in placement fiexbitiry and"))
