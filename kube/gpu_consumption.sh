312a4a1dc9641842e32999ff71c8034bd817be14
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-24 12:21:51 -0700

    new analytics

--- kube/gpu_consumption.sh
+++ kube/gpu_consumption.sh
@@ -7,20 +7,22 @@ tmux new-session -d -s gpu-info
 window=0
 for node in $gpu_nodes; do
     if [ $window -ne 0 ]; then
-        tmux split-window -h
+        tmux new-window -t gpu-info
     fi
+    
+    tmux rename-window -t gpu-info:$window "$node"
     
     pod=$(kubectl get pods -n chunkmydocs --field-selector spec.nodeName=$node -o jsonpath='{.items[0].metadata.name}')
     
     if [ -z "$pod" ]; then
-        tmux send-keys -t gpu-info:0.$window "echo 'No GPU pod found on node $node'" C-m
+        tmux send-keys -t gpu-info:$window "echo 'No GPU pod found on node $node'" C-m
     else
-        tmux send-keys -t gpu-info:0.$window "echo 'Node: $node, Pod: $pod'" C-m
-        tmux send-keys -t gpu-info:0.$window "kubectl exec -it $pod -n chunkmydocs -- watch -n 0.5 nvidia-smi" C-m
+        tmux send-keys -t gpu-info:$window "echo 'Node: $node, Pod: $pod'" C-m
+        tmux send-keys -t gpu-info:$window "kubectl exec -it $pod -n chunkmydocs -- watch -n 0.5 nvidia-smi" C-m
     fi
     
     window=$((window + 1))
 done
 
-tmux select-layout tiled
+tmux select-window -t gpu-info:0
 tmux attach-session -t gpu-info
