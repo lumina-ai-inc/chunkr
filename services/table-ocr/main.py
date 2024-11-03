fdbf9d93c4ed6f38bf596cf93c6c5fba1ea47380
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-11-03 00:28:32 -0700

    wokring on table ocr

--- services/table-ocr/main.py
+++ services/table-ocr/main.py
@@ -1,8 +1,35 @@
 import argparse
+from io import BytesIO
+from paddlex import create_model
+from PIL import Image
 from robyn import Robyn
+import tempfile
 
 app = Robyn(__file__)
 
+model = create_model("SLANet_plus")
+
+@app.get("/")
+def health(request):
+    return "OK"
+
+
+@app.post("/ocr")
+def extract(request):
+    files = request.files
+    if not files:
+        print("No files provided in the request.")
+        return {"error": "No files provided."}
+    file_key, file_content = next(iter(files.items()))
+    image = Image.open(BytesIO(file_content))
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
+        image.save(temp_file.name)
+        output = model.predict(temp_file.name, batch_size=1)
+    for res in output:
+        res.print(json_format=False)
+        res.save_to_img("./output/")
+        res.save_to_json("./output/res.json")
+    return res
 
 
 if __name__ == "__main__":
