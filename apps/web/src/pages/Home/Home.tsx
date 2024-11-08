689e5199290960737d73395351a900819aea5130
Mehul Chadda <mehul@lumina.sh>
2024-11-08 15:59:23 -0800

    Landing page improvements

--- apps/web/src/pages/Home/Home.tsx
+++ apps/web/src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
-import { Flex, ScrollArea, Text } from "@radix-ui/themes";
+import { useEffect } from "react";
+import { Flex, Text } from "@radix-ui/themes";
 import { useAuth } from "react-oidc-context";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import Header from "../../components/Header/Header";
-import UploadMain from "../../components/Upload/UploadMain";
+// import UploadMain from "../../components/Upload/UploadMain";
 import Footer from "../../components/Footer/Footer";
-import heroImageWebp from "../../assets/hero/hero-image.webp";
-import heroImageJpg from "../../assets/hero/hero-image-85-p.jpg";
+// import heroImageWebp from "../../assets/hero/hero-image.webp";
+// import heroImageJpg from "../../assets/hero/hero-image-85-p.jpg";
 
 const Home = () => {
   const auth = useAuth();
@@ -21,6 +22,36 @@ const Home = () => {
     }
   };
 
+  useEffect(() => {
+    const button = document.querySelector(".signup-button");
+
+    const handleMouseMove = (e: MouseEvent) => {
+      const rect = (e.target as HTMLElement)?.getBoundingClientRect();
+      const x = e.clientX - rect.left; // x position within the element
+      const y = e.clientY - rect.top; // y position within the element
+
+      // Convert to percentage
+      const xPercent = (x / rect.width) * 100;
+      const yPercent = (y / rect.height) * 100;
+
+      (button as HTMLElement)?.style.setProperty("--mouse-x", `${xPercent}%`);
+      (button as HTMLElement)?.style.setProperty("--mouse-y", `${yPercent}%`);
+    };
+
+    if (button) {
+      button.addEventListener("mousemove", handleMouseMove as EventListener);
+    }
+
+    return () => {
+      if (button) {
+        button.removeEventListener(
+          "mousemove",
+          handleMouseMove as EventListener
+        );
+      }
+    };
+  }, []);
+
   return (
     <Flex
       direction="column"
@@ -29,12 +60,12 @@ const Home = () => {
         height: "100%",
         width: "100%",
       }}
-      className="pulsing-background"
+      className="background"
     >
       <Flex className="header-container">
         <div
           style={{
-            maxWidth: "1312px",
+            maxWidth: "1424px",
             width: "100%",
             height: "fit-content",
           }}
@@ -54,65 +85,52 @@ const Home = () => {
           <Header px="0px" home={true} />
         </div>
       </Flex>
-      <ScrollArea type="scroll">
-        <div>
-          <div className="hero-main-container">
-            <div className="hero-image-container fade-in">
-              <picture>
-                <source srcSet={heroImageWebp} type="image/webp" />
-                <img
-                  src={heroImageJpg}
-                  alt="hero"
-                  className="hero-image"
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                  }}
-                />
-              </picture>
-              <div className="hero-gradient-overlay"></div>
-            </div>
-            <div className="hero-content-container">
-              <Flex className="hero-container">
-                <Flex className="text-container" direction="column">
-                  {/* <Flex direction="row" gap="16px">
-                    <Text
-                      size="3"
-                      weight="bold"
-                      style={{ color: "hsl(0, 0%, 100%, 0.98)" }}
-                    >
-                      Backed by Y Combinator
-                    </Text>
-                  </Flex> */}
+      <div style={{ height: "100%" }}>
+        <div className="hero-main-container">
+          <div className="hero-content-container">
+            <Flex className="hero-container" align="center" justify="center">
+              <Flex
+                className="text-container"
+                direction="column"
+                align="center"
+                justify="center"
+              >
+                <Flex direction="row" gap="16px" className="yc-tag">
                   <Text
-                    size="9"
-                    weight="bold"
-                    trim="both"
-                    className="hero-title"
-                    mb="24px"
+                    size="2"
+                    weight="regular"
+                    style={{ color: "hsl(0, 0%, 100%, 0.9)" }}
                   >
-                    Open Source Data Ingestion
+                    Backed by Y Combinator
                   </Text>
-                  <Text
-                    weight="medium"
-                    size="5"
-                    className="white hero-description"
-                  >
-                    API service for document layout analysis, OCR and chunking
-                    to convert documents into RAG & LLM-ready data.
-                  </Text>
+                </Flex>
+                <Text
+                  weight="bold"
+                  className="hero-title"
+                  mb="16px"
+                  align="center"
+                >
+                  Open Source Document Ingestion
+                </Text>
+                <Text
+                  weight="regular"
+                  size="3"
+                  className="hero-description"
+                  align="center"
+                >
+                  API service to turn complex documents into RAG/LLM-ready data
+                </Text>
 
-                  <Text
+                {/* <Text
                     weight="bold"
                     size="4"
                     mb="12px"
                     className="white signup-byline"
                   >
                     We support PDF, DOC, PPT, and XLS files.
-                  </Text>
+                  </Text> */}
 
-                  <Flex direction="row" gap="24px" mb="40px" wrap="wrap">
+                {/* <Flex direction="row" gap="24px" mb="40px" wrap="wrap">
                     <Flex
                       direction="row"
                       gap="8px"
@@ -265,78 +283,37 @@ const Home = () => {
                         Handwriting
                       </Text>
                     </Flex>
-                  </Flex>
-                  <Flex
-                    className="signup-container"
-                    direction="row"
-                    gap="16px"
-                    align="center"
-                  >
-                    <button
-                      className="signup-button"
-                      onClick={handleGetStarted}
-                    >
-                      {isAuthenticated ? (
-                        <Text size="3" weight="bold">
-                          Go to dashboard
-                        </Text>
-                      ) : (
-                        <Text size="3" weight="bold">
-                          Get started for free
-                        </Text>
-                      )}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                      >
-                        <rect
-                          width="24"
-                          height="24"
-                          fill="white"
-                          fillOpacity="0.01"
-                        />
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M13.0343 5.03431C13.3467 4.72188 13.8532 4.72188 14.1656 5.03431L20.5657 11.4343C20.878 11.7467 20.878 12.2533 20.5657 12.5657L14.1656 18.9658C13.8532 19.2781 13.3467 19.2781 13.0343 18.9658C12.7218 18.6533 12.7218 18.1467 13.0343 17.8342L18.0686 12.8H3.99995C3.55813 12.8 3.19995 12.4418 3.19995 12C3.19995 11.5582 3.55813 11.2 3.99995 11.2H18.0686L13.0343 6.16567C12.7218 5.85326 12.7218 5.34673 13.0343 5.03431Z"
-                          fill="#000000"
-                        />
-                      </svg>
-                    </button>
-                    {!isAuthenticated && (
-                      <Text
-                        size="1"
-                        weight="bold"
-                        style={{ color: "hsl(0, 0%, 100%, 0.95)" }}
-                        className="signup-button-text"
-                      >
-                        1500 pages in credits
+                  </Flex> */}
+                <Flex
+                  className="signup-container"
+                  direction="row"
+                  gap="16px"
+                  align="center"
+                >
+                  <button className="signup-button" onClick={handleGetStarted}>
+                    {isAuthenticated ? (
+                      <Text size="5" weight="bold">
+                        Go to dashboard
+                      </Text>
+                    ) : (
+                      <Text size="5" weight="bold">
+                        Get started for free
                       </Text>
                     )}
-                  </Flex>
-                </Flex>
-                <Flex className="module-container" direction="column" gap="4">
-                  <UploadMain isAuthenticated={isAuthenticated} />
+                  </button>
                 </Flex>
               </Flex>
-            </div>
+              {/* <Flex className="module-container" direction="column" gap="4">
+                  <UploadMain isAuthenticated={isAuthenticated} />
+                </Flex> */}
+            </Flex>
           </div>
         </div>
+      </div>
 
-        <Footer />
-      </ScrollArea>
+      <Footer />
     </Flex>
   );
 };
 
 export default Home;
-
-// curl -X POST https://api.chunkmydocs.com/api/task \ <br></br>
-// -H "Content-Type: application/json" \ <br></br>
-// -H "Authorization:{"{your_api_key}"}" \ <br></br>
-// -F "file=@/path/to/your/file.pdf" \ <br></br>
-// -F "model=Fast" \ <br></br>
-// -F "target_chunk_length=512"
