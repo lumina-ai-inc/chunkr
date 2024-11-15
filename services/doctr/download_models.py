92971d7266fb779b8ece412eb1114eedd2f44538
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-11-15 14:36:00 -0800

    docker not working

---
+++ services/doctr/download_models.py
@@ -0,0 +1,6 @@
+from doctr.models import ocr_predictor
+
+if __name__ == "__main__":
+    predictor = ocr_predictor('fast_base', 'crnn_vgg16_bn', pretrained=True, 
+                             export_as_straight_boxes=True).cuda()
+    print(predictor)
