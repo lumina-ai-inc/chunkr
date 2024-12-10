5b3c1a5ee06b260bee2c2569c7249df6c93c31de
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-12-09 22:18:03 -0800

    cloudflare removed dsn records

--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -8,15 +8,3 @@ variable "cloudflare_account_id" {
   description = "Cloudflare account ID"
   type        = string
 }
-
-variable "cloudflare_zone_id" {
-  description = "Cloudflare zone ID"
-  type        = string
-  default     = ""
-}
-
-variable "domain" {
-  description = "Your domain name"
-  type        = string
-  default     = "chunkr.ai"
-}
