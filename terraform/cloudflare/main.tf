c9ccdb330703139e8930cc7fa32b00ecb1f38993
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-12-09 22:18:03 -0800

    cloudflare removed dsn records

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -26,55 +26,6 @@ resource "cloudflare_tunnel" "chunkr_tunnel" {
 }
 
 ###############################################################
-# DNS Records - Optional
-###############################################################
-
-resource "cloudflare_record" "root" {
-  count   = var.cloudflare_zone_id != "" ? 1 : 0
-  zone_id = var.cloudflare_zone_id
-  name    = "@"
-  content = "${cloudflare_tunnel.chunkr_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  proxied = true
-}
-
-resource "cloudflare_record" "www" {
-  count   = var.cloudflare_zone_id != "" ? 1 : 0
-  zone_id = var.cloudflare_zone_id
-  name    = "www"
-  content = "${cloudflare_tunnel.chunkr_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  proxied = true
-}
-
-resource "cloudflare_record" "api" {
-  count   = var.cloudflare_zone_id != "" ? 1 : 0
-  zone_id = var.cloudflare_zone_id
-  name    = "api"
-  content = "${cloudflare_tunnel.chunkr_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  proxied = true
-}
-
-resource "cloudflare_record" "auth" {
-  count   = var.cloudflare_zone_id != "" ? 1 : 0
-  zone_id = var.cloudflare_zone_id
-  name    = "auth"
-  content = "${cloudflare_tunnel.chunkr_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  proxied = true
-}
-
-resource "cloudflare_record" "rrq" {
-  count   = var.cloudflare_zone_id != "" ? 1 : 0
-  zone_id = var.cloudflare_zone_id
-  name    = "rrq"
-  content = "${cloudflare_tunnel.chunkr_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  proxied = true
-}
-
-###############################################################
 # Outputs
 ###############################################################
 
@@ -91,20 +42,4 @@ output "tunnel_name" {
 output "tunnel_cname" {
   description = "CNAME target for the tunnel"
   value       = "${cloudflare_tunnel.chunkr_tunnel.id}.cfargotunnel.com"
-}
-
-output "domain_configured" {
-  description = "Whether domain records were configured"
-  value       = var.cloudflare_zone_id != ""
-}
-
-output "dns_records" {
-  description = "Map of configured DNS records (only if domain is configured)"
-  value = var.cloudflare_zone_id != "" ? {
-    root = "https://${var.domain}"
-    www  = "https://www.${var.domain}"
-    api  = "https://api.${var.domain}"
-    auth = "https://auth.${var.domain}"
-    rrq  = "https://rrq.${var.domain}"
-  } : null
 }
