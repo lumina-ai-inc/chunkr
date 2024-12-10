ea901ba38d5cc3f7ef9279005461a5471d531eda
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-12-09 21:56:50 -0800

    cloudflare dns for tunnel and dns records

--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -3,3 +3,20 @@ variable "cloudflare_api_token" {
   type        = string
   sensitive   = true
 }
+
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID"
+  type        = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare zone ID"
+  type        = string
+  default     = ""
+}
+
+variable "domain" {
+  description = "Your domain name"
+  type        = string
+  default     = "chunkr.ai"
+}
