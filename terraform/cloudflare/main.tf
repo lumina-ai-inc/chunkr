24cf1d39ab73caf5bddf757f01036997b45276c1
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-12-09 21:37:19 -0800

    adding support for cloudflare tunnel

---
+++ terraform/cloudflare/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
