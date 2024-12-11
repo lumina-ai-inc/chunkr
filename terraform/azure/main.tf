406207f3e39117febd7d6680083fc2dc26ef3952
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-12-11 11:25:02 -0800

    kubernetes on azure

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -117,7 +117,6 @@ resource "azurerm_subnet" "services_subnet" {
   }
 }
 
-// ... existing code ...
 
 ###############################################################
 # Network Security Group
@@ -198,7 +197,7 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_container" "container" {
-  name                  = "${var.base_name}-container"
+  name                  = var.base_name
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
