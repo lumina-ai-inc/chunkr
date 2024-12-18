76ac9aeb72ee33f763defcec6a01ab6a9a9424b5
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-12-18 15:18:51 -0800

    updated backend config

--- terraform/backend/backend.example.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "YOUR_BUCKET_NAME"
-    key            = "chunkr/azure/terraform.tfstate"
-    region         = "YOUR_REGION"
-    encrypt        = true
-    dynamodb_table = "YOUR_DYNAMODB_TABLE"
-  }
-} 
