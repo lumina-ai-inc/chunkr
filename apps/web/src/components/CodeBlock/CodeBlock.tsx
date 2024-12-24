da1f87c057ee42acdd8456d3772b994911204f6c
Mehul Chadda <mehul@lumina.sh>
2024-12-23 20:59:12 -0800

    Updates to pricing section and home layout

--- apps/web/src/components/CodeBlock/CodeBlock.tsx
+++ apps/web/src/components/CodeBlock/CodeBlock.tsx
@@ -5,6 +5,7 @@ import "prismjs/themes/prism-tomorrow.css"; // or another theme
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-bash";
+import "prismjs/components/prism-rust";
 import "./CodeBlock.css";
 
 interface CodeBlockProps {
