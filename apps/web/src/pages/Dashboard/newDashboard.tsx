af223c656df486bb6b3a121e4c6c17b38d6f4348
Mehul Chadda <mehul@lumina.sh>
2025-01-05 17:37:11 -0800

    Small fixes

--- apps/web/src/pages/Dashboard/newDashboard.tsx
+++ apps/web/src/pages/Dashboard/newDashboard.tsx
@@ -452,7 +452,7 @@ export default function NewDashboard() {
         <Flex className="main-header">
           <Flex gap="8px" align="center">
             <Flex onClick={handleReturnToTasks} style={{ cursor: "pointer" }}>
-              <Text size="5" weight="medium" style={{ color: "#FFF" }}>
+              <Text size="5" weight="medium" className="main-header-text">
                 Your Tasks
               </Text>
             </Flex>
@@ -483,61 +483,6 @@ export default function NewDashboard() {
           </Flex>
           <Flex gap="24px">
             <UploadDialog auth={auth} />
-            <BetterButton>
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 25 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clip-path="url(#clip0_113_1479)">
-                  <path
-                    d="M19.75 9.25V20.25C19.75 20.8 19.3 21.25 18.75 21.25H6.25C5.7 21.25 5.25 20.8 5.25 20.25V3.75C5.25 3.2 5.7 2.75 6.25 2.75H13.25"
-                    stroke="#FFF"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M13.25 9.25H19.75L13.25 2.75V9.25Z"
-                    stroke="#FFF"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M10 15.25L12.5 12.75L15 15.25"
-                    stroke="#FFF"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M12.5 13.75V18.25"
-                    stroke="#FFF"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_113_1479">
-                    <rect
-                      width="24"
-                      height="24"
-                      fill="white"
-                      transform="translate(0.5)"
-                    />
-                  </clipPath>
-                </defs>
-              </svg>
-
-              <Text size="2" weight="medium" style={{ color: "#FFF" }}>
-                Create Task
-              </Text>
-            </BetterButton>
-
             <BetterButton>
               <svg
                 width="18"
