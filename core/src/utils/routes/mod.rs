d23f5557ab3f94b56e3d5beb1494f7def6bb9225
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-01-07 19:25:19 -0800

    chunkr v1.1.0 services added

--- core/src/utils/routes/mod.rs
+++ core/src/utils/routes/mod.rs
@@ -1,6 +1,6 @@
-pub mod get_task;
-pub mod create_task;
-pub mod get_user;
-pub mod create_user;
 pub mod admin_user;
+pub mod create_task;
+pub mod create_user;
+pub mod get_task;
 pub mod get_tasks;
+pub mod get_user;
