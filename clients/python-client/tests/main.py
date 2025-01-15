049879422a7188c026dda346b6d20a084f472ac0
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-01-15 13:50:33 -0800

    update works

---
+++ clients/python-client/tests/main.py
@@ -0,0 +1,26 @@
+from chunkr_ai import Chunkr
+from chunkr_ai.models import Configuration, SegmentationStrategy, OcrStrategy, SegmentProcessing, GenerationConfig, GenerationStrategy
+
+def main():
+    chunkr = Chunkr()
+    task_id = "702e910e-c01a-4287-84b4-2b759d3c943e"
+    # new_config = Configuration(
+    #     segmentation_strategy=SegmentationStrategy.PAGE,
+    #     segment_processing=SegmentProcessing(
+    #         page=GenerationConfig(
+    #             html=GenerationStrategy.LLM,
+    #             markdown=GenerationStrategy.LLM,
+    #         )
+    #     )
+    # )
+    new_config = Configuration(
+        segmentation_strategy=SegmentationStrategy.LAYOUT_ANALYSIS,
+    )
+    try:
+        task = chunkr.update(task_id, new_config)
+        print(task)
+    except Exception as e:
+        print(e)
+if __name__ == "__main__":
+    main()
+
