7e971aca7bc37bcdca2f1d4cd73b7195318e8bc2
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-01-19 18:57:29 -0800

    updated task worker to accept azure as pipeline type

---
+++ core/src/pipeline/chunking.rs
@@ -0,0 +1,41 @@
+use crate::models::chunkr::output::Segment;
+use crate::models::chunkr::pipeline::Pipeline;
+use crate::models::chunkr::task::Status;
+use crate::utils::services::chunking;
+
+/// Chunk the segments
+///
+/// This function will perform chunking on the segments
+pub async fn process(pipeline: &mut Pipeline) -> Result<(), Box<dyn std::error::Error>> {
+    pipeline
+        .get_task()?
+        .update(
+            Some(Status::Processing),
+            Some("Chunking".to_string()),
+            None,
+            None,
+            None,
+            None,
+        )
+        .await?;
+
+    let segments: Vec<Segment> = pipeline
+        .output
+        .chunks
+        .clone()
+        .into_iter()
+        .map(|c| c.segments)
+        .flatten()
+        .collect();
+
+    let chunk_processing = pipeline.get_task()?.configuration.chunk_processing.clone();
+
+    let chunks = chunking::hierarchical_chunking(
+        segments,
+        chunk_processing.target_length,
+        chunk_processing.ignore_headers_and_footers,
+    )?;
+
+    pipeline.output.chunks = chunks;
+    Ok(())
+}
