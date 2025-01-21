06f2c471f9dccd0805f6f8b6f3c08df8e27ca506
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-01-21 15:06:35 -0800

    added vite runtime env

--- apps/analytics/vite.config.ts
+++ apps/analytics/vite.config.ts
@@ -1,12 +1,8 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import runtimeEnv from "vite-plugin-runtime-env";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  plugins: [react(), runtimeEnv()],
+});
