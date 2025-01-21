1b7a6d278ae6ad4489d67f825efe92fb36389f94
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-01-21 15:11:13 -0800

    analytics succesfully deployed

--- apps/analytics/vite.config.ts
+++ apps/analytics/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "path"
+import react from "@vitejs/plugin-react"
 import runtimeEnv from "vite-plugin-runtime-env";
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), runtimeEnv()],
-});
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
