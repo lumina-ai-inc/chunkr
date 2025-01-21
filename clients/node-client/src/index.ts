5219c932f6ec57e72519f8b489e5fac851e5aa84
Mehul Chadda <mehul@lumina.sh>
2025-01-21 14:32:35 -0800

    Basic readme and stable package - building tests with package

--- clients/node-client/src/index.ts
+++ clients/node-client/src/index.ts
@@ -1,11 +1,27 @@
 export { Chunkr } from "./Chunkr";
 export { TaskResponse } from "./models/TaskResponse";
-export { Configuration, Status, OcrStrategy } from "./models/Configuration";
+export {
+  Configuration,
+  JsonSchema,
+  Property,
+  OcrStrategy,
+  ChunkProcessing,
+  SegmentProcessing,
+  SegmentationStrategy,
+  GenerationStrategy,
+  GenerationConfig,
+  CroppingStrategy,
+} from "./models/Configuration";
 export type {
   TaskResponseData,
+  Status,
   Output,
   Segment,
   Chunk,
   BoundingBox,
+  OCRResult,
+  SegmentType,
+  ExtractedField,
+  ExtractedJson,
 } from "./models/TaskResponseData";
 export type { ClientConfig } from "./models/ClientConfig";
