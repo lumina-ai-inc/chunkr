3303caf09d5d582e85d447fc9da8a0dc14fb1c50
Mehul Chadda <mehul@lumina.sh>
2025-01-20 17:30:43 -0800

    Added switch for Azure pipeline to Upload component

--- apps/web/src/components/Upload/UploadMain.tsx
+++ apps/web/src/components/Upload/UploadMain.tsx
@@ -6,6 +6,7 @@ import {
   SegmentationStrategy,
   DEFAULT_UPLOAD_CONFIG,
   DEFAULT_SEGMENT_PROCESSING,
+  Pipeline,
 } from "../../models/taskConfig.model";
 import "./UploadMain.css";
 import Upload from "./Upload";
@@ -111,6 +112,39 @@ export default function UploadMain({
           className={`config-section ${!isAuthenticated ? "disabled" : ""}`}
         >
           <div className="config-grid">
+            <ToggleGroup
+              label={
+                <Flex gap="2" align="center">
+                  <svg
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 16 16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                  >
+                    <path
+                      fill="#FFF"
+                      fill-rule="evenodd"
+                      d="M2.75 2.5A1.75 1.75 0 001 4.25v1C1 6.216 1.784 7 2.75 7h1a1.75 1.75 0 001.732-1.5H6.5a.75.75 0 01.75.75v3.5A2.25 2.25 0 009.5 12h1.018c.121.848.85 1.5 1.732 1.5h1A1.75 1.75 0 0015 11.75v-1A1.75 1.75 0 0013.25 9h-1a1.75 1.75 0 00-1.732 1.5H9.5a.75.75 0 01-.75-.75v-3.5A2.25 2.25 0 006.5 4H5.482A1.75 1.75 0 003.75 2.5h-1zM2.5 4.25A.25.25 0 012.75 4h1a.25.25 0 01.25.25v1a.25.25 0 01-.25.25h-1a.25.25 0 01-.25-.25v-1zm9.75 6.25a.25.25 0 00-.25.25v1c0 .138.112.25.25.25h1a.25.25 0 00.25-.25v-1a.25.25 0 00-.25-.25h-1z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <span>Pipeline</span>
+                </Flex>
+              }
+              value={config.pipeline || "Default"}
+              onChange={(value) =>
+                setConfig({
+                  ...config,
+                  pipeline:
+                    value === "Default" ? undefined : (value as Pipeline),
+                })
+              }
+              options={[
+                { label: "Default", value: "Default" },
+                { label: "Azure", value: Pipeline.Azure },
+              ]}
+            />
             <ToggleGroup
               label={
                 <Flex gap="2" align="center">
