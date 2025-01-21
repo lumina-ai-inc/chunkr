5fb482e6d1570b22690641a2aee5276843dd9d34
Mehul Chadda <mehul@lumina.sh>
2025-01-21 14:26:31 -0800

    Reworked some models and exposed all models to user

--- clients/node-client/src/index.ts
+++ clients/node-client/src/index.ts
@@ -1,1 +1,11 @@
-
+export { Chunkr } from "./Chunkr";
+export { TaskResponse } from "./models/TaskResponse";
+export { Configuration, Status, OcrStrategy } from "./models/Configuration";
+export type {
+  TaskResponseData,
+  Output,
+  Segment,
+  Chunk,
+  BoundingBox,
+} from "./models/TaskResponseData";
+export type { ClientConfig } from "./models/ClientConfig";
