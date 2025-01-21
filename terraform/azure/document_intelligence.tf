7111541ff1e29003195a6e7aa71f5f4d1d83ccc7
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-01-21 14:36:52 -0800

    added entreprise kube

---
+++ terraform/azure/document_intelligence.tf
@@ -0,0 +1,16 @@
+resource "azurerm_cognitive_account" "document_intelligence" {
+  name                = "${var.base_name}-docint"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FormRecognizer"
+  sku_name            = "S0"
+}
+
+output "document_intelligence_endpoint" {
+  value = azurerm_cognitive_account.document_intelligence.endpoint
+}
+
+output "document_intelligence_key" {
+  value     = azurerm_cognitive_account.document_intelligence.primary_access_key
+  sensitive = true
+}
