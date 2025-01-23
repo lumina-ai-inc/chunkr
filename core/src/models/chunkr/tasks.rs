a39be0442c69285de2f9bbd2d681be7c9720492d
Ishaan <ishaan99@gmail.com>
2025-01-23 22:21:07 +0000

    get tasks

--- core/src/models/chunkr/tasks.rs
+++ core/src/models/chunkr/tasks.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-#[derive(Debug, Serialize, Deserialize, Clone, ToSchema)]
+#[derive(Deserialize)]
 pub struct TasksQuery {
     pub page: Option<i64>,
     pub limit: Option<i64>,
     pub include_output: Option<bool>,
+    pub start: Option<chrono::DateTime<chrono::Utc>>,
+    pub end: Option<chrono::DateTime<chrono::Utc>>,
 }
