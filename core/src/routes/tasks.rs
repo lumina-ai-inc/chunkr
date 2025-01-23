a473b556aa6b3cffd209c2770eed0a113f1ce146
Ishaan <ishaan99@gmail.com>
2025-01-23 22:21:07 +0000

    get tasks

--- core/src/routes/tasks.rs
+++ core/src/routes/tasks.rs
@@ -1,10 +1,8 @@
 use crate::models::chunkr::auth::UserInfo;
 use crate::models::chunkr::task::TaskResponse;
 use crate::models::chunkr::tasks::TasksQuery;
-use crate::utils::routes::get_tasks::get_task_details;
 use crate::utils::routes::get_tasks::get_tasks;
 use actix_web::{web, Error, HttpResponse};
-use serde::{Deserialize, Serialize};
 
 /// Get Tasks
 ///
@@ -35,25 +33,12 @@ pub async fn get_tasks_route(
     user_info: web::ReqData<UserInfo>,
 ) -> Result<HttpResponse, Error> {
     let page = query.page.unwrap_or(1);
-    let limit = query.limit.unwrap_or(10);
-    let include_output = query.include_output.unwrap_or(false);
-    let tasks = get_tasks(user_info.user_id.clone(), page, limit, include_output).await?;
-    Ok(HttpResponse::Ok().json(tasks))
-}
-
-#[derive(Deserialize)]
-pub struct TaskDetailsQuery {
-    start: Option<chrono::DateTime<chrono::Utc>>,
-    end: Option<chrono::DateTime<chrono::Utc>>,
-}
-
-pub async fn get_task_details_route(
-    query: web::Query<TaskDetailsQuery>,
-    user_info: web::ReqData<UserInfo>,
-) -> Result<HttpResponse, Error> {
-    let start = query.start.unwrap_or_else(chrono::Utc::now);
+    let limit = query.limit.unwrap_or(20);
+    let start = query
+        .start
+        .unwrap_or_else(|| chrono::Utc::now() - chrono::Duration::days(30));
     let end = query.end.unwrap_or_else(chrono::Utc::now);
-    let email = user_info.email.as_deref();
-    let task_details = get_task_details(start, end, email).await?;
-    Ok(HttpResponse::Ok().json(task_details))
+
+    let tasks = get_tasks(user_info.user_id.clone(), page, limit, false, start, end).await?;
+    Ok(HttpResponse::Ok().json(tasks))
 }
