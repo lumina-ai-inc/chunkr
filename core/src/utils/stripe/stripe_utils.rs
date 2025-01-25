82f98b80a2ce705ee714a650ee43125fad968696
Ishaan Kapoor <ishaan99@gmail.com>
2025-01-24 17:21:51 -0800

    sub cost

--- core/src/utils/stripe/stripe_utils.rs
+++ core/src/utils/stripe/stripe_utils.rs
@@ -242,9 +242,10 @@ pub async fn create_stripe_billing_portal_session(
 ) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
     let client = ReqwestClient::new();
 
+    let return_url = format!("{}/dashboard", stripe_config.return_url.trim_end_matches('/'));
     let form_data = vec![
         ("customer", customer_id),
-        ("return_url", &stripe_config.return_url),
+        ("return_url", &return_url),
     ];
 
     let stripe_response = client
