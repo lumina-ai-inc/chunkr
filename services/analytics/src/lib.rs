e7fa294c57cf9344836b42423b60675e19b6bc9d
Ishaan Kapoor <ishaan99@gmail.com>
2025-01-31 13:42:12 -0800

    new shas

--- services/analytics/src/lib.rs
+++ services/analytics/src/lib.rs
@@ -34,7 +34,7 @@ pub async fn main() -> std::io::Result<()> {
                     .configure(crate::routes::routes::configure_routes)
             )
     })
-    .bind(("0.0.0.0", 8069))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
 }
