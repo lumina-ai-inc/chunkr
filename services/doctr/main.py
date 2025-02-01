4100f858a402e0884900511d6239ee75a5f2789e
Ishaan Kapoor <ishaan99@gmail.com>
2025-02-01 13:20:18 -0800

    fixing compose

--- services/doctr/main.py
+++ services/doctr/main.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", default="0.0.0.0", help="Host to bind to")
-    parser.add_argument("--port", type=int, default=8002, help="Port to bind to") 
+    parser.add_argument("--port", type=int, default=8000, help="Port to bind to") 
     args = parser.parse_args()
 
     uvicorn.run(app, host=args.host, port=args.port)
