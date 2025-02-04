ebb8e620a2b6023243a77035fb19631b2c84e3fe
Ishaan Kapoor <ishaan99@gmail.com>
2025-02-03 17:52:10 -0800

    deploy

--- services/doctr/download_models.py
+++ services/doctr/download_models.py
@@ -1,5 +1,5 @@
 from doctr.models import ocr_predictor
 
 if __name__ == "__main__":
-    predictor = ocr_predictor('db_resnet50', 'vitstr_small', pretrained=True, 
+    predictor = ocr_predictor('db_resnet50', 'parseq', pretrained=True, 
                              export_as_straight_boxes=True)
