ce4473959f2f7a697ec4ad33a0c6d9928fab1c58
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-02-07 16:42:08 -0800

    added new models

--- core/src/models/chunkr/upload_multipart.rs
+++ core/src/models/chunkr/upload_multipart.rs
@@ -4,101 +4,82 @@ use crate::models::chunkr::segment_processing::SegmentProcessing;
 use crate::models::chunkr::task::Configuration;
 #[cfg(feature = "azure")]
 use crate::models::chunkr::task::PipelineType;
-use postgres_types::{FromSql, ToSql};
-use serde::{Deserialize, Serialize};
-use strum_macros::{Display, EnumString};
+use crate::models::chunkr::upload::{OcrStrategy, SegmentationStrategy};
+use actix_multipart::form::json::Json as MPJson;
+use actix_multipart::form::{tempfile::TempFile, MultipartForm};
 use utoipa::{IntoParams, ToSchema};
 
-#[derive(
-    Debug, Serialize, Deserialize, PartialEq, Clone, ToSql, FromSql, ToSchema, Display, EnumString,
-)]
-/// Controls the Optical Character Recognition (OCR) strategy.
-/// - `All`: Processes all pages with OCR. (Latency penalty: ~0.5 seconds per page)
-/// - `Auto`: Selectively applies OCR only to pages with missing or low-quality text. When text layer is present the bounding boxes from the text layer are used.
-pub enum OcrStrategy {
-    All,
-    #[serde(alias = "Off")]
-    Auto,
-}
-
-impl Default for OcrStrategy {
-    fn default() -> Self {
-        OcrStrategy::All
-    }
-}
-
-#[derive(
-    Serialize,
-    Deserialize,
-    Debug,
-    Clone,
-    Display,
-    EnumString,
-    Eq,
-    PartialEq,
-    ToSql,
-    FromSql,
-    ToSchema,
-)]
-/// Controls the segmentation strategy:
-/// - `LayoutAnalysis`: Analyzes pages for layout elements (e.g., `Table`, `Picture`, `Formula`, etc.) using bounding boxes. Provides fine-grained segmentation and better chunking. (Latency penalty: ~TBD seconds per page).
-/// - `Page`: Treats each page as a single segment. Faster processing, but without layout element detection and only simple chunking.
-pub enum SegmentationStrategy {
-    LayoutAnalysis,
-    Page,
-}
-
-impl Default for SegmentationStrategy {
-    fn default() -> Self {
-        SegmentationStrategy::LayoutAnalysis
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, ToSchema, IntoParams)]
+#[derive(Debug, MultipartForm, ToSchema, IntoParams)]
+#[into_params(parameter_in = Query)]
 pub struct CreateForm {
-    pub chunk_processing: Option<ChunkProcessing>,
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = Option<ChunkProcessing>, format = "binary")]
+    pub chunk_processing: Option<MPJson<ChunkProcessing>>,
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = Option<i32>, format = "binary")]
     /// The number of seconds until task is deleted.
     /// Expried tasks can **not** be updated, polled or accessed via web interface.
-    pub expires_in: Option<i32>,
-    /// The file to be uploaded. Can be a URL or a base64 encoded file.
-    pub file: String,
+    pub expires_in: Option<MPJson<i32>>,
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = String, format = "binary")]
+    /// The file to be uploaded.
+    pub file: TempFile,
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = Option<bool>, default = false, format = "binary")]
     /// Whether to use high-resolution images for cropping and post-processing. (Latency penalty: ~7 seconds per page)
-    #[schema(default = false)]
-    pub high_resolution: Option<bool>,
-    #[schema(default = "All")]
-    pub ocr_strategy: Option<OcrStrategy>,
+    pub high_resolution: Option<MPJson<bool>>,
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = Option<OcrStrategy>, default = "All", format = "binary")]
+    pub ocr_strategy: Option<MPJson<OcrStrategy>>,
     #[cfg(feature = "azure")]
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = Option<PipelineType>, format = "binary")]
     /// The PipelineType to use for processing.
     /// If pipeline is set to Azure then Azure layout analysis will be used for segmentation and OCR.
     /// The output will be unified to the Chunkr `output` format.
-    pub pipeline: Option<PipelineType>,
-    pub segment_processing: Option<SegmentProcessing>,
-    #[schema(default = "LayoutAnalysis")]
-    pub segmentation_strategy: Option<SegmentationStrategy>,
+    pub pipeline: Option<MPJson<PipelineType>>,
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = Option<SegmentProcessing>, format = "binary")]
+    pub segment_processing: Option<MPJson<SegmentProcessing>>,
+    #[param(style = Form, value_type = String, format = "binary")]
+    #[schema(value_type = Option<SegmentationStrategy>, default = "LayoutAnalysis", format = "binary")]
+    pub segmentation_strategy: Option<MPJson<SegmentationStrategy>>,
 }
 
 impl CreateForm {
     fn get_chunk_processing(&self) -> ChunkProcessing {
         self.chunk_processing
-            .clone()
+            .as_ref()
+            .map(|mp_json| mp_json.0.clone())
             .unwrap_or(ChunkProcessing::default())
     }
 
     fn get_expires_in(&self) -> Option<i32> {
         let job_config = job_config::Config::from_env().unwrap();
-        self.expires_in.or(job_config.expiration_time)
+        self.expires_in
+            .as_ref()
+            .map(|e| e.0)
+            .or(job_config.expiration_time)
     }
 
     fn get_high_resolution(&self) -> bool {
-        self.high_resolution.unwrap_or(false)
+        self.high_resolution.as_ref().map(|e| e.0).unwrap_or(false)
     }
 
     fn get_ocr_strategy(&self) -> OcrStrategy {
-        self.ocr_strategy.clone().unwrap_or_default()
+        self.ocr_strategy
+            .as_ref()
+            .map(|e| e.0.clone())
+            .unwrap_or(OcrStrategy::default())
     }
 
     fn get_segment_processing(&self) -> SegmentProcessing {
-        let user_config = self.segment_processing.clone().unwrap_or_default();
+        let user_config = self
+            .segment_processing
+            .as_ref()
+            .map(|e| e.0.clone())
+            .unwrap_or_default();
+
         SegmentProcessing {
             title: user_config
                 .title
@@ -140,12 +121,15 @@ impl CreateForm {
     }
 
     fn get_segmentation_strategy(&self) -> SegmentationStrategy {
-        self.segmentation_strategy.clone().unwrap_or_default()
+        self.segmentation_strategy
+            .as_ref()
+            .map(|e| e.0.clone())
+            .unwrap_or(SegmentationStrategy::default())
     }
 
     #[cfg(feature = "azure")]
     fn get_pipeline(&self) -> Option<PipelineType> {
-        self.pipeline.clone()
+        self.pipeline.as_ref().map(|e| e.0.clone())
     }
 
     pub fn to_configuration(&self) -> Configuration {
@@ -166,27 +150,46 @@ impl CreateForm {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, ToSchema, IntoParams)]
+#[derive(Debug, MultipartForm, ToSchema, IntoParams)]
+#[into_params(parameter_in = Query)]
 pub struct UpdateForm {
-    pub chunk_processing: Option<ChunkProcessing>,
+    #[param(style = Form, value_type = Option<ChunkProcessing>, format = "binary")]
+    #[schema(value_type = Option<ChunkProcessing>, format = "binary")]
+    pub chunk_processing: Option<MPJson<ChunkProcessing>>,
+    #[param(style = Form, value_type = Option<i32>, format = "binary")]
+    #[schema(value_type = Option<i32>, format = "binary")]
     /// The number of seconds until task is deleted.
     /// Expried tasks can **not** be updated, polled or accessed via web interface.
-    pub expires_in: Option<i32>,
+    pub expires_in: Option<MPJson<i32>>,
+    #[param(style = Form, value_type = Option<bool>, format = "binary")]
+    #[schema(value_type = Option<bool>, format = "binary")]
     /// Whether to use high-resolution images for cropping and post-processing. (Latency penalty: ~7 seconds per page)
-    pub high_resolution: Option<bool>,
-    pub ocr_strategy: Option<OcrStrategy>,
+    pub high_resolution: Option<MPJson<bool>>,
+    #[param(style = Form, value_type = Option<OcrStrategy>, format = "binary")]
+    #[schema(value_type = Option<OcrStrategy>, format = "binary")]
+    pub ocr_strategy: Option<MPJson<OcrStrategy>>,
     #[cfg(feature = "azure")]
+    #[param(style = Form, value_type = Option<PipelineType>, format = "binary")]
+    #[schema(value_type = Option<PipelineType>, format = "binary")]
     /// The pipeline to use for processing.
     /// If pipeline is set to Azure then Azure layout analysis will be used for segmentation and OCR.
     /// The output will be unified to the Chunkr output.
-    pub pipeline: Option<PipelineType>,
-    pub segment_processing: Option<SegmentProcessing>,
-    pub segmentation_strategy: Option<SegmentationStrategy>,
+    pub pipeline: Option<MPJson<PipelineType>>,
+    #[param(style = Form, value_type = Option<SegmentProcessing>, format = "binary")]
+    #[schema(value_type = Option<SegmentProcessing>, format = "binary")]
+    pub segment_processing: Option<MPJson<SegmentProcessing>>,
+    #[param(style = Form, value_type = Option<SegmentationStrategy>, format = "binary")]
+    #[schema(value_type = Option<SegmentationStrategy>, format = "binary")]
+    pub segmentation_strategy: Option<MPJson<SegmentationStrategy>>,
 }
 
 impl UpdateForm {
     fn get_segment_processing(&self, current_config: &Configuration) -> SegmentProcessing {
-        let user_config = self.segment_processing.clone().unwrap_or_default();
+        let user_config = self
+            .segment_processing
+            .as_ref()
+            .map(|e| e.0.clone())
+            .unwrap_or_default();
 
         SegmentProcessing {
             title: user_config
@@ -232,25 +235,34 @@ impl UpdateForm {
         Configuration {
             chunk_processing: self
                 .chunk_processing
-                .clone()
+                .as_ref()
+                .map(|e| e.0.clone())
                 .unwrap_or_else(|| current_config.chunk_processing.clone()),
-            expires_in: self.expires_in.or(current_config.expires_in),
+            expires_in: self
+                .expires_in
+                .as_ref()
+                .map(|e| e.0)
+                .or(current_config.expires_in),
             high_resolution: self
                 .high_resolution
+                .as_ref()
+                .map(|e| e.0)
                 .unwrap_or(current_config.high_resolution),
             input_file_url: None,
             json_schema: None,
             model: None,
             ocr_strategy: self
                 .ocr_strategy
-                .clone()
+                .as_ref()
+                .map(|e| e.0.clone())
                 .unwrap_or(current_config.ocr_strategy.clone()),
             #[cfg(feature = "azure")]
-            pipeline: self.pipeline.clone(),
+            pipeline: self.pipeline.as_ref().map(|e| e.0.clone()),
             segment_processing: self.get_segment_processing(current_config),
             segmentation_strategy: self
                 .segmentation_strategy
-                .clone()
+                .as_ref()
+                .map(|e| e.0.clone())
                 .unwrap_or(current_config.segmentation_strategy.clone()),
             target_chunk_length: None,
         }
