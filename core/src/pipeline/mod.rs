1c6cfd9c65a84b84748599d57fc28156e542ea6b
Akhilesh Sharma <sharmaninenine@gmail.com>
2025-02-19 14:17:59 -0800

    Azure as default pipeline

--- core/src/pipeline/mod.rs
+++ core/src/pipeline/mod.rs
@@ -1,8 +1,7 @@
+pub mod azure;
 pub mod chunking;
 pub mod convert_to_images;
 pub mod crop;
 pub mod segment_processing;
 pub mod segmentation_and_ocr;
 // pub mod structured_extraction;
-
-pub mod azure;
